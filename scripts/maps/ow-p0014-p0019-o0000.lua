e468f855de8d02712b104c12d7e7e5c68ed611e7
seeseekey <seeseekey@784bf6b6-0765-11df-962c-b5c252e50527>
2010-03-08 13:58:05 +0000

    Vorbereitungen zur Erweiterung der Weltkarte.
    git-svn-id: https://invertika.googlecode.com/svn/trunk@1322 784bf6b6-0765-11df-962c-b5c252e50527

---
+++ scripts/maps/ow-p0014-p0019-o0000.lua
@@ -0,0 +1,25 @@
+----------------------------------------------------------------------------------
+-- Map File                                                                     --
+--                                                                              --
+-- In dieser Datei stehen die entsprechenden externen NPC's, Trigger und        --
+-- anderer Dinge.                                                               --
+--                                                                              --
+----------------------------------------------------------------------------------
+--  Copyright 2010 The Invertika Development Team                               --
+--                                                                              --
+--  This file is part of Invertika.                                             --
+--                                                                              --
+--  Invertika is free software; you can redistribute it and/or modify it        --
+--  under the terms of the GNU General  Public License as published by the Free --
+--  Software Foundation; either version 2 of the License, or any later version. --
+----------------------------------------------------------------------------------
+
+
+require "data/scripts/libs/npclib"
+
+
+dofile("data/scripts/ivklibs/warp.lua")
+
+atinit(function()
+ create_inter_map_warp_trigger(2035, 2085, 2033, 1983) --- Intermap warp
+end)
