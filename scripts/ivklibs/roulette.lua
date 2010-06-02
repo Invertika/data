a82336d0c74bfa39ae899974f8f625bb00bcbb4b
original-andy@gmx.de <original-andy@gmx.de@784bf6b6-0765-11df-962c-b5c252e50527>
2010-06-02 20:38:51 +0000

    Roulette-Script: Null-Feld heisst jetzt "0"
    git-svn-id: https://invertika.googlecode.com/svn/trunk@1457 784bf6b6-0765-11df-962c-b5c252e50527

--- scripts/ivklibs/roulette.lua
+++ scripts/ivklibs/roulette.lua
@@ -71,7 +71,7 @@ function roulette_talk(npc, ch)
     if v == 1 then w = do_choice(npc, ch,"Rot","Schwarz")
     elseif v == 2 then w = do_choice(npc, ch,"Gleich","Ungleich")
     elseif v == 3 then w = do_choice(npc, ch,
-      "Grün","1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", 
+      "0","1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", 
       "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", 
       "25", "26", "27", "28", "29", "30", "31", "32", "33", "34", "35", "36")
     w = w - 1
