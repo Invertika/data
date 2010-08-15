9dadeb9a8b0f96f0656508825822e71fecfd1352
seeseekey <seeseekey@784bf6b6-0765-11df-962c-b5c252e50527>
2010-08-15 19:27:51 +0000

    Passende Skripte zur Tischmap.
    git-svn-id: https://invertika.googlecode.com/svn/trunk@1705 784bf6b6-0765-11df-962c-b5c252e50527

--- scripts/maps/ow-n0001-n0002-o0000.lua
+++ scripts/maps/ow-n0001-n0002-o0000.lua
@@ -20,4 +20,46 @@ dofile("data/scripts/ivklibs/warp.lua")
 
 atinit(function()
  create_inter_map_warp_trigger(1, 57, 3, 13) --- Intermap warp
+
+ create_npc("Jurkan", 29, 80 * TILESIZE + 16, 115 * TILESIZE + 16, jurkan_talk, nil) --- Jurkan
+ create_npc("Holzbein", 38, 83 * TILESIZE + 16, 115 * TILESIZE + 16, holzbein_talk, nil) --- Holzbein
+ create_npc("seeseekey", 20, 93 * TILESIZE + 16, 118 * TILESIZE + 16, seeseekey_talk, nil) --- seeseekey
+ create_npc("Delegan", 11, 81 * TILESIZE + 16, 122 * TILESIZE + 16, delegan_talk, nil) --- Delegan
 end)
+
+function jurkan_talk(npc, ch)
+	do_message(npc, ch, invertika.get_random_element("Irgendwie ist mir zu heiß hier...",
+	  "Glaub mir, das hier war nicht geplant.",
+	  "Das habe ich nun davon, seeseekey geholfen zu haben. Wer das ist? Der da drüben!",
+	  "ICH habe euch geschaffen, das mit Invertika ist alles Quatsch!!!!!",
+	  "Warum anderen das Leben schwermachen, wenn man es ihnen nehmen kann?",
+	  "Könnte mir vielleicht nochmal jemand was zu trinken bringen? Mit viel Eis?",
+	  "Ich steh hier nur so rum, das ist schöpferische Willkür.",
+	  "Nicht zu fassen. Überall diese Dingsbums.",
+	  "while true; do echo Blubb; done; Blubb blubb blubb blubb blubb...",
+	  "Seit ich hier in der Wüste bin, habe ich Schweissfüße!"))
+	  do_npc_close(npc, ch)
+end
+
+function holzbein_talk(npc, ch)
+	do_message(npc, ch, invertika.get_random_element("Wo ist mein Kamel?",
+	  "Lieber Arm dran, als Arm ab.",
+	  "Steck den Spaltenvektor in die Vektorspalte.",
+	  "ICH habe euch geschaffen, das mit Invertika ist alles Quatsch!!!!!",
+	  "Um Pirat zu werden, musst du schon mehr machen als CDs brennen!",
+	  "Der Tisch ist neidisch auf mein Holzbein.",
+	  "Wo ist meine Brille?",
+	  "Was hat das zu bedeuten?"))
+	  do_npc_close(npc, ch)
+end
+
+function delegan_talk(npc, ch)
+	do_message(npc, ch, invertika.get_random_element("Ich bin nicht implementiert. Na und?"))
+	do_npc_close(npc, ch)
+end
+
+
+
+
+
+
