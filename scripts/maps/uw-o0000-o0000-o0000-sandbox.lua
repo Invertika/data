1a7e9d01a61fd6792f2776f9f9172d10c42236bc
Jurkan@gmx.de <Jurkan@gmx.de@784bf6b6-0765-11df-962c-b5c252e50527>
2010-08-18 12:50:29 +0000

    validatemaps.py verbessert
    git-svn-id: https://invertika.googlecode.com/svn/trunk@1735 784bf6b6-0765-11df-962c-b5c252e50527

--- scripts/maps/uw-o0000-o0000-o0000-sandbox.lua
+++ scripts/maps/uw-o0000-o0000-o0000-sandbox.lua
@@ -16,17 +16,49 @@
 
 require "scripts/lua/npclib"
 require "scripts/ivklibs/invertika"
+---require "scripts/ivklibs/trap"
 
 dofile("data/scripts/ivklibs/warp.lua")
 
 atinit(function()
  create_inter_map_warp_trigger(19003, 19003, 19003, 19003) --- Intermap warp
- 
- --- Zelan
  create_npc("Zelan", 58, 132 * TILESIZE + 16, 21 * TILESIZE + 16, zelan_talk, nil) --- Zelan
+ mana.trigger_create(20 * TILESIZE, 20 * TILESIZE, 22 * TILESIZE, 22 * TILESIZE, "trap_trigger_skorpions", 0, true) --- Trigger Trap
 end)
 
 function zelan_talk(npc, ch)
     do_message(npc, ch, "Wo du bist? Im Vacare. Jeder neue kommt hier her bevor es raus geht in die große Welt. Also pass auf dich auf.")
 	do_npc_close(npc, ch)
+end
+
+
+--- Falle auslösen
+function trap_trigger_skorpions(ch, id)
+  trap(ch, 21*TILESIZE, 21*TILESIZE,TILESIZE,TILESIZE,10)--- Bibliotheksfunktion
+end
+
+--- Bibliotheksfunktion
+function trap(ch, x, y, width, height, monsterid)
+  if (mana.being_type(ch) ~= TYPE_MONSTER) then --- Nur Player löst Falle aus
+   local x1 = x - width/2
+   local y1 = y - height/2
+   local x2 = x + width/2
+   local y2 = y + height/2
+   local currentTime = os.time(t)
+   local lastTime = get_quest_var(ch, "trap_last_activated")
+
+   if (lastTime == nil) or (lastTime == "") then
+     mana.chr_set_quest(ch, "trap_last_activated", currentTime)      
+     print("trap case1: trap triggered for first time")
+   elseif math.floor(currentTime-lastTime) > 20 then
+     mana.chr_set_quest(ch, "trap_last_activated", currentTime)      
+     --local spawned_monster = mana.monster_create(monsterid, x, y)
+     --mana.being_say(spawned_monster,"Du bist in einen Hinterhalt geraten!")
+     print("trap case2: create monster")
+   else
+     print("trap case3: be patient")
+     --- nichts passiert
+   end
+
+ end
 end
