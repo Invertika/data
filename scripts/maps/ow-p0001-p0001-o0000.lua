3ac0e1d0b23862fa14bfd4094799e6c1d6587198
seeseekey <seeseekey@784bf6b6-0765-11df-962c-b5c252e50527>
2010-10-07 10:28:05 +0000

    Grabstein beschriftet und Netheksäule hinzugefügt.
    git-svn-id: https://invertika.googlecode.com/svn/trunk@2453 784bf6b6-0765-11df-962c-b5c252e50527

--- scripts/maps/ow-p0001-p0001-o0000.lua
+++ scripts/maps/ow-p0001-p0001-o0000.lua
@@ -15,9 +15,19 @@
 ----------------------------------------------------------------------------------
 
 require "scripts/lua/npclib"
+require "scripts/ivklibs/invertika"
+require "scripts/ivklibs/nethek"
 
 dofile("data/scripts/ivklibs/warp.lua")
 
 atinit(function()
+ create_npc("Grabstein", 31, 70 * TILESIZE + 16, 39 * TILESIZE + 16, grabstein_talk, nil) --- Grabstein
+
  create_inter_map_warp_trigger(74, 84, 72, 62) --- Intermap warp
+ nethek.create_netheksaeule(139 * TILESIZE, 43 * TILESIZE + 16) --- Netheksäule
 end)
+
+function grabstein_talk(npc, ch)
+	do_message(npc, ch, "Ayume\n12430-12470\n\nIn Trauer: Saya und Tetse")
+	do_npc_close(npc, ch)
+end
