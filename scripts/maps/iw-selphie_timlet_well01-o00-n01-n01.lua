f786af4e6685c7f4821a36588577af201aad11b8
Jurkan@gmx.de <Jurkan@gmx.de@784bf6b6-0765-11df-962c-b5c252e50527>
2010-10-10 06:46:47 +0000

    Einige Ta No ins HQ hinzugefügt
    git-svn-id: https://invertika.googlecode.com/svn/trunk@2472 784bf6b6-0765-11df-962c-b5c252e50527

--- scripts/maps/iw-selphie_timlet_well01-o00-n01-n01.lua
+++ scripts/maps/iw-selphie_timlet_well01-o00-n01-n01.lua
@@ -19,16 +19,21 @@ require "scripts/lua/npclib"
 require "scripts/ivklibs/invertika"
 
 atinit(function()
- create_npc("Blubb", 89, 35 * TILESIZE + 16, 44 * TILESIZE + 16, tano1_talk, npclib.walkaround_wide) --- Ta No 1
- wache = create_npc("Schatzwächter", 89, 42 * TILESIZE + 16, 36 * TILESIZE + 16, tano2_talk, nil) --- Ta No 2 (Schatzkammer Wächter)
- create_npc("Karl", 89, 25 * TILESIZE + 16, 41 * TILESIZE + 16, tano3_talk, nil) --- Ta No 3
+ create_npc("Irdath", 89, 35 * TILESIZE + 16, 44 * TILESIZE + 16, tano1_talk, npclib.walkaround_small) --- Ta No 1 (Ta No am Eingang)
+ wache = create_npc("Rhaor", 91, 42 * TILESIZE + 16, 36 * TILESIZE + 16, tano2_talk, nil) --- Ta No 2 (Schatzkammer Wächter)
+ create_npc("Karl", 92, 25 * TILESIZE + 16, 41 * TILESIZE + 16, tano3_talk, nil) --- Ta No 3 (Verkäufer)
+ create_npc("Samos", 93, 67 * TILESIZE + 16, 14 * TILESIZE + 16, tano4_talk, nil) --- Ta No 4 (Sektenführer)
  
  mana.trigger_create(42 * TILESIZE, 36 * TILESIZE, 64, 64, "treasure_trap", 0, true)
  mana.trigger_create(39 * TILESIZE, 36 * TILESIZE, 96, 96, "treasure_warning", 0, true)
 end)
 
 function tano1_talk(npc, ch)
-    
+    do_message(npc, ch, invertika.get_random_element("Geh doch mal zu Samos, der hat bestimmt einen Auftrag für dich.",
+      "Im Laden kannst du erst etwas kaufen, wenn du dir Reputation bei uns erworben hast.",
+      "Was das hier ist? Natürlich das Hauptquartier der Ta No!",
+      "Unsere Ziele? Die sind geheim!"))
+    do_npc_close(npc, ch)
 end
 
 function tano2_talk(npc, ch)
@@ -41,6 +46,11 @@ end
 
 function tano3_talk(npc, ch)
     do_message(npc, ch, "Ich werde irgendwann mal Waffen und Ausrüstung verkaufen und so.")
+    do_npc_close(npc, ch)
+end
+
+function tano4_talk(npc, ch)
+    do_message(npc, ch, "Ich soll irgendwann mal Quests vergeben, mit denen die Reputation bei den Ta No gesteigert werden kann.")
     do_npc_close(npc, ch)
 end
 
@@ -61,6 +71,6 @@ function treasure_warning(being)
   if (mana.being_type(being) == TYPE_CHARACTER) then --- Nur Spieler beachten
     mana.being_say(wache, invertika.get_random_element("Keinen Schritt weiter!",
       "Stopp! Sonst wird es schmerzhaft.",
-      "Halt!")
+      "Halt!"))
   end
 end
