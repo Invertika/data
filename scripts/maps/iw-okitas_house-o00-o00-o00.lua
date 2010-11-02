4a116cf2fd0517784d7e06fd25728d3be2d3b15f
seeseekey <seeseekey@784bf6b6-0765-11df-962c-b5c252e50527>
2010-11-02 18:08:27 +0000

    Tetse und Saya implementiert.
    git-svn-id: https://invertika.googlecode.com/svn/trunk@2587 784bf6b6-0765-11df-962c-b5c252e50527

--- scripts/maps/iw-okitas_house-o00-o00-o00.lua
+++ scripts/maps/iw-okitas_house-o00-o00-o00.lua
@@ -19,4 +19,24 @@ require "scripts/lua/npclib"
 
 
 atinit(function()
+
+ create_npc("Saya", 7, 43 * TILESIZE + 16, 15 * TILESIZE + 16, saya_talk, nil) --- Saya
+ create_npc("Tetse", 138, 45 * TILESIZE + 16, 23 * TILESIZE + 16, tetse_talk, nil) --- Tetse
 end)
+
+function saya_talk(npc, ch)
+	do_message(npc, ch, invertika.get_random_element("Oh wir haben Gäste. Tetse deckst du denn Tisch ich bin fast fertig.",
+	  "Tetse lernte ich vor sehr langer Zeit kennen.",
+	  "Wir haben hier alles was wir brauchen.",
+	  "Okita ist vor einiger Zeit gegangen, er sagte er müsse einige Dinge klären."))
+	  do_npc_close(npc, ch)
+end
+
+function tetse_talk(npc, ch)
+	do_message(npc, ch, invertika.get_random_element("Ja Saya, ich decke gleich den Tisch. Ich muss nur noch schnell mein Katana schärfen.",
+	  "Sei willkommen Gast. Gleich gibt es etwas zu essen.",
+	  "Du suchst Okita? Er ist schon lange Zeit nicht mehr hier gewesen. Ich glaube er wollte zur Küste.",
+	  "Ja der Sandsturm wütet. Das geht schon die ganze Zeit so.",
+	  "Warst du an ihrem Grab?"))
+	  do_npc_close(npc, ch)
+end
