2a444bb4660ae6b0de0710172f95164f1098cdfb
Jurkan@gmx.de <Jurkan@gmx.de@784bf6b6-0765-11df-962c-b5c252e50527>
2010-11-14 19:51:01 +0000

    Banker-Bug behoben
    git-svn-id: https://invertika.googlecode.com/svn/trunk@2674 784bf6b6-0765-11df-962c-b5c252e50527

--- scripts/ivklibs/banker.lua
+++ scripts/ivklibs/banker.lua
@@ -146,7 +146,7 @@ function banker_talk(npc, ch)
 		elseif v2 == 7 then --- 50000
 			bank_get_money_from_account_dlg(npc, ch, 50000)
 		elseif v2 == 8 then --- Alles
-			bank_pay_money_to_account_dlg(npc, ch, bank_get_account_balance(ch))
+			bank_get_money_from_account_dlg(npc, ch, bank_get_account_balance(ch))
 		end
 	elseif v == 4 then --- Kredit aufnehmen
 		do_message(npc, ch, "Kredite werden noch nicht vergeben!")
