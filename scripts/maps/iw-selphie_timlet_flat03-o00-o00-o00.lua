b25851102efe351084c9b39dde8465b1a47e4441
seeseekey <seeseekey@784bf6b6-0765-11df-962c-b5c252e50527>
2010-12-26 13:01:39 +0000

    Tresorquest eingebaut.
    git-svn-id: https://invertika.googlecode.com/svn/trunk@2956 784bf6b6-0765-11df-962c-b5c252e50527

--- scripts/maps/iw-selphie_timlet_flat03-o00-o00-o00.lua
+++ scripts/maps/iw-selphie_timlet_flat03-o00-o00-o00.lua
@@ -17,5 +17,50 @@
 require "scripts/lua/npclib"
 
 atinit(function()
- ---create_npc("Banker", 122, 180 * TILESIZE + 16, 160 * TILESIZE + 16, banker.banker_talk, nil) --- Banker (Debug)
+ create_npc("...", 1, 68 * TILESIZE + 16, 18 * TILESIZE + 16, tresor_talk, nil) --- Tresor
 end)
+
+function tresor_talk(npc, ch)
+	-- quest init
+	if tonumber(get_quest_var(ch, "selphi_timlet_flat03_safe")) == nil then
+	  mana.chr_set_quest(ch, "selphi_timlet_flat03_safe", 0)
+	end
+  
+	-- quest get/set functions
+	function get_qstatus() return tonumber(get_quest_var(ch, "selphi_timlet_flat03_safe")) end
+	function set_qstatus(x) mana.chr_set_quest(ch, "selphi_timlet_flat03_safe", tonumber(x)) end
+  
+	if get_qstatus()==0 then
+	  do_message(npc, ch, "Hinter dem Bild befindet sich etwas. Mal schauen. Oh das ist ein Tresor. Mmm da schaue ich mal hinein. Bloß wie bekomme ich ihn auf?")
+	
+	  while true do 
+	  	  local v = do_choice(npc, ch, "Dynamit benutzen", "Nett anklopfen", "Zweimal links, einmal rechts, dreimal diagonal, koxiales Relais + Solantriebskala Stufe 10 x Pi, Kartoffel und Schnitt-Lauch im Sinn...", "42", "Die Zahlenkombination auf der Rückseite des Bildes benutzen")
+								   
+		  if v == 1 then
+			do_message(npc, ch, "Ähm ich habe gar kein Dynamit.")
+			--TODO Dynamit als Item einbauen
+			--set_qstatus(2)
+			break
+		  elseif v == 2 then
+			do_message(npc, ch, "Nein, bin gerade beschäftigt. Inventur. Bitte kommen sie während unserer Servicezeiten nochmal.")
+			break
+		  elseif v == 3 then
+			do_message(npc, ch, "Da wir uns zu jeder Zeit, gleichzeitig im Universum befinden und alles Wissen erlangen das es gibt, wissen wir das diese Option die falsche war.")
+			break
+		  elseif v == 4 then
+			do_message(npc, ch, "Wie jetzt? Das kann nicht dein Ernst sein.")
+			break
+		  elseif v == 5 then
+			do_message(npc, ch, "Ein Zettel. Mal sehen was draufsteht: Diebstahl lohnt sich nicht.")
+			set_qstatus(1)
+			break
+		  end
+	  end
+	elseif get_qstatus()==1 then
+	  do_message(npc, ch, "Der Tresor ist offen und leer.")
+	elseif get_qstatus()==2 then
+	  do_message(npc, ch, "Der Tresor ist durch die Explosion verzogen und lässt sich nicht mehr öffnen.")
+	end
+	
+	do_npc_close(npc, ch)
+end
