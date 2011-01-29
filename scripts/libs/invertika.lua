bf17d9739f9367901ff48a1fecda9bb4a6999638
Jurkan@gmx.de <Jurkan@gmx.de@784bf6b6-0765-11df-962c-b5c252e50527>
2011-01-29 16:45:32 +0000

    Ein paar Funktionen in die Invertika-Lib hinzugefügt (EXP, Geld, Items ändern)
    git-svn-id: https://invertika.googlecode.com/svn/trunk@3206 784bf6b6-0765-11df-962c-b5c252e50527

--- scripts/libs/invertika.lua
+++ scripts/libs/invertika.lua
@@ -1,7 +1,7 @@
 ----------------------------------------------------------------------------------
 -- Invertika Modul - Grundfunktionen                                            --
 --                                                                              --
--- Dieses Modul stellt Grundfunktionen zur Verfügung					        --
+-- Dieses Modul stellt Grundfunktionen zur Verfügung                            --
 --                                                                              --
 --                                                                              --
 ----------------------------------------------------------------------------------
@@ -20,10 +20,14 @@ require "scripts/lua/npclib"
 
 --- Gibt ein zufällig eines der übergebenen Argumente zurück
 function get_random_element(...)
-  local arrayCount = table.getn( arg ) -- Anzahl der Elemente  ermitteln
-  local r = math.random(1, arrayCount)
-  return arg[r]
+    local arrayCount = table.getn( arg ) -- Anzahl der Elemente  ermitteln
+    local r = math.random(1, arrayCount)
+    return arg[r]
 end
+
+---------------------------
+-- Queststati            --
+---------------------------
 
 -- Initialisisert einen Queststatus
 function init_quest_status(ch, questname)
@@ -39,5 +43,53 @@ end
 
 -- Gibt einen Queststatus zurück
 function get_quest_status(ch, questname)
-  return tonumber(get_quest_var(ch, questname))
+    return tonumber(get_quest_var(ch, questname))
+end
+
+---------------------------
+-- EXP/Skill-Änderungen  --
+---------------------------
+
+local MONEY_ADD_TEXT = "Du hast %s Aki erhalten!"
+local MONEY_REMOVE_TEXT = "Du hast %s Aki weniger!"
+local ITEM_ADD_TEXT = "Du hast %sx %s erhalten!"
+local ITEM_REMOVE_TEXT = "Du hast %sx %s weniger!"
+
+-- Experimentell und unfertig
+function add_exp(character, attribute, amount, optimalLevel)
+    mana.chr_give_exp(character, attribute, amount, optimalLevel)
+    mana.chatmessage(character, "Du hast " + amount + "Erfahrungspunkte erhalten!")
+end
+
+-- Fügt Character ch amount Aki hinzu (bei negativen Werten wird abgezogen)
+function add_money(ch, amount)
+    local current_money = mana.chr_money(ch)
+    if current_money + amount < 0 then
+        error("add_money failed: money would be less than 0")
+    else
+        mana.chr_money_change(ch, current_money + amount)
+        if amount > 0 then
+            mana.chatmessage(ch, string.format(MONEY_ADD_TEXT, amount))
+        else
+            mana.chatmessage(ch, string.format(MONEY_REMOVE_TEXT, amount*-1))
+        end
+    end
+end
+
+-- Fügt Charakter ch numer Items der ID id hinzu
+-- name sollte auf den Itemnamen gesetzt werden,
+-- damit die Message vom Server richtig aussieht
+function add_items(ch, id, number, name)
+    local success = mana.chr_inv_change(ch, id, number)
+    if success then
+        if number > 0 then
+            mana.chatmessage(ch, string.format(ITEM_ADD_TEXT, number, name))
+        elseif number < 0 then
+            mana.chatmessage(ch, string.format(ITEM_REMOVE_TEXT, number, name))
+        else
+            -- 0 Items werden hinzugefügt/geändert
+            -- Aber wer sollte so etwas tun?
+        end
+    end
+    return success
 end
