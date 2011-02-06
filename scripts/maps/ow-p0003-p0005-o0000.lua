b74220558e37099510af06c3b29276a6060d1166
seeseekey <seeseekey@784bf6b6-0765-11df-962c-b5c252e50527>
2011-02-06 15:32:58 +0000

    Haus NPCs hinzugefügt.
    git-svn-id: https://invertika.googlecode.com/svn/trunk@3390 784bf6b6-0765-11df-962c-b5c252e50527

--- scripts/maps/ow-p0003-p0005-o0000.lua
+++ scripts/maps/ow-p0003-p0005-o0000.lua
@@ -20,7 +20,10 @@ dofile("data/scripts/libs/warp.lua")
 
 atinit(function()
  create_inter_map_warp_trigger(19001, 110, 98, 88) --- Intermap warp
- create_npc("Regret", 8, 55 * TILESIZE + 16, 173 * TILESIZE + 16, regret_talk, nil) --- Nepuret 
+ create_npc("Regret", 8, 55 * TILESIZE + 16, 173 * TILESIZE + 16, regret_talk, nil) --- Regret 
+ 
+ create_npc("Holbert", 63, 129 * TILESIZE + 16, 120 * TILESIZE + 16, holbert_talk, nil) --- Holbert
+ create_npc("Vektor", 11, 134 * TILESIZE + 16, 120 * TILESIZE + 16, vektor_talk, nil) --- Vektor
 end)
 
 function regret_talk(npc, ch)
@@ -28,5 +31,21 @@ function regret_talk(npc, ch)
 	    "Ich muss doch nach Leo. Wo bin ich? In Alexia? Gibt es auf unserem Kontinent nicht.",
 	    "Ich muss jetzt echt los sonst komme ich zu spät zu der Fortsetzung meiner Spielereihe.",
 	    "Wenn ich den kriege der mir das hier eingebrockt hat."))
+	do_npc_close(npc, ch)
+end
+
+function holbert_talk(npc, ch)
+	do_message(npc, ch, invertika.get_random_element("Immer diese Entwickler... berücksichtigen die elementaren Grundlagen der Physik nicht. Und was haben wir nun davon? Dieses Haus.",
+	    "Von wegen optische Täuschung, das haben die wirklich so gebaut!",
+	    "Nichts ist unmöglich... Invertika *mir Werbemusik dazu denke*",
+	    "Möchte mal wissen wer die Baugenehmigung dafür ausgestellt hat."))
+	do_npc_close(npc, ch)
+end
+
+function vektor_talk(npc, ch)
+	do_message(npc, ch, invertika.get_random_element("Ich war früher mal ein Superschurke.",
+	    "Ich bin Statiker, aber damit habe ich nichts, aber auch wirklich garnichts zu tun.",
+	    "Eigentlich müsste dieses Haus in sich zusammenfallen. Oder das Universum. Blöderweise passiert nichts von beidem.",
+	    "Wir könnten auch einfach so tun, als ob da gar kein Haus steht."))
 	do_npc_close(npc, ch)
 end
