9f17ff906e9edcdf79752771d11a0ecff871598a
erikschilling@freenet.de <erikschilling@freenet.de@784bf6b6-0765-11df-962c-b5c252e50527>
2011-02-14 17:52:02 +0000

    Poker: Player: Methoden hinzugefügt um die Zeit zu messen wie lange der Spieler am Zug ist.
    git-svn-id: https://invertika.googlecode.com/svn/trunk@3486 784bf6b6-0765-11df-962c-b5c252e50527

--- scripts/libs/poker/pokerplayer.lua
+++ scripts/libs/poker/pokerplayer.lua
@@ -4,6 +4,7 @@ PokerPlayer = {}
 
 PokerPlayer.ch = nil
 PokerPlayer.spade = nil
+PokerPlayer.time_rounde_start = nil
 
 --- Erstellt eine neue Instanz der Klasse PokerPlayer
 function PokerPlayer:new(ch)
@@ -32,4 +33,24 @@ end
 function PokerPlayer:doPayment(pot, amount)
     self:removeMoney(amount)
     pot:doPayment(self, amount)
+end
+
+--- Signalisiert dem Spieler, dass er am Zug ist.
+function PokerPlayer:startTurn()
+    self.time_round_start = os.time(t)
+end
+
+--- Singalisiert dem Spieler, dass sein Zug beendet ist.
+function PokerPlayer:endTurn()
+    self.time_round_start = nil
+end
+
+--- Gibt die Zeit zurück die verstrichen ist seit der Spieler am Zug ist.
+-- @return Zeit seit Beginn der Zuges.
+function PokerPlayer:getTimePlayerIsOnTurn()
+    if self.time_round_start == nil then
+        return 0
+    else
+        return self.time_round_start - os.time(t)
+    end
 end
