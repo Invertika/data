561556a8cc6e30268f99dc31a07189a9888c62c1
seeseekey <seeseekey@784bf6b6-0765-11df-962c-b5c252e50527>
2011-02-23 09:24:10 +0000

    Modulbezeichnung korrigiert (falsch benannt).
    git-svn-id: https://invertika.googlecode.com/svn/trunk@3578 784bf6b6-0765-11df-962c-b5c252e50527

--- scripts/libs/poker.lua
+++ scripts/libs/poker.lua
@@ -1,20 +1,4 @@
-----------------------------------------------------------------------------------
--- Invertika Modul - Grundfunktionen                                            --
---                                                                              --
--- Dieses Modul stellt Grundfunktionen zur Verfügung                            --
---                                                                              --
---                                                                              --
-----------------------------------------------------------------------------------
---  Copyright 2010 The Invertika Development Team                               --
---                                                                              --
---  This file is part of Invertika.                                             --
---                                                                              --
---  Invertika is free software; you can redistribute it and/or modify it        --
---  under the terms of the GNU General  Public License as published by the Free --
---  Software Foundation; either version 2 of the License, or any later version. --
-----------------------------------------------------------------------------------
-
-module("invertika", package.seeall)
+module("poker", package.seeall)
 
 require "scripts/lua/npclib"
 
