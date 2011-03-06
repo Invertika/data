cca9281f0f1988269ba57b18289e2a22135f0751
Jurkan@gmx.de <Jurkan@gmx.de@784bf6b6-0765-11df-962c-b5c252e50527>
2011-03-06 18:39:20 +0000

    Anfang eines weiteren Teils der Wüstenhöhle hinzugefügt
    git-svn-id: https://invertika.googlecode.com/svn/trunk@3709 784bf6b6-0765-11df-962c-b5c252e50527

---
+++ scripts/maps/iw-desert-cave01-o00-n01-n01.lua
@@ -0,0 +1,21 @@
+----------------------------------------------------------------------------------
+-- Map File                                                                     --
+--                                                                              --
+-- In dieser Datei stehen die entsprechenden externen NPC's, Trigger und        --
+-- anderer Dinge.                                                               --
+--                                                                              --
+----------------------------------------------------------------------------------
+--  Copyright 2011 - 2011 by Invertika Project                                  --
+--                                                                              --
+--  This file is part of Invertika.                                             --
+--                                                                              --
+--  Invertika is free software; you can redistribute it and/or modify it        --
+--  under the terms of the GNU General  Public License as published by the Free --
+--  Software Foundation; either version 2 of the License, or any later version. --
+----------------------------------------------------------------------------------
+
+require "scripts/lua/npclib"
+require "scripts/libs/invertika"
+
+atinit(function()
+end)
