e0607cba8cf16e63440a55e65c834be0091b888d
seeseekey <seeseekey@784bf6b6-0765-11df-962c-b5c252e50527>
2011-03-20 14:32:43 +0000

    Kleinere Änderungen.
    git-svn-id: https://invertika.googlecode.com/svn/trunk@3861 784bf6b6-0765-11df-962c-b5c252e50527

--- scripts/maps/ow-p0001-n0005-o0000.lua
+++ scripts/maps/ow-p0001-n0005-o0000.lua
@@ -15,9 +15,66 @@
 ----------------------------------------------------------------------------------
 
 require "scripts/lua/npclib"
+require "scripts/libs/datetime"
+require "scripts/libs/invertika"
+require "scripts/libs/nethek"
+require "scripts/libs/sign"
 
 dofile("data/scripts/libs/warp.lua")
 
 atinit(function()
  create_inter_map_warp_trigger(70, 82, 19001, 60) --- Intermap warp
+ nethek.create_netheksaeule(95 * TILESIZE, 113 * TILESIZE + 16) --- Netheksäule
+ 
+ -- Schilder
+ local signText="Sperrgebiet! Betreten sowie Fotographie verboten! Wachen sind zur Anwendung tötlicher Gewalt befugt!"
+ sign.create_sign(78, 100, signText) -- Warnschild
+ sign.create_sign(78, 93, signText) -- Warnschild
+ sign.create_sign(78, 86, signText) -- Warnschild
+ sign.create_sign(78, 79, signText) -- Warnschild
+ sign.create_sign(78, 72, signText) -- Warnschild
+ sign.create_sign(86, 72, signText) -- Warnschild
+ sign.create_sign(96, 72, signText) -- Warnschild
+ sign.create_sign(106, 72, signText) -- Warnschild
+ sign.create_sign(115, 72, signText) -- Warnschild
+ sign.create_sign(126, 72, signText) -- Warnschild
+ sign.create_sign(135, 72, signText) -- Warnschild
+ sign.create_sign(135, 78, signText) -- Warnschild
+ sign.create_sign(135, 84, signText) -- Warnschild
+ sign.create_sign(135, 92, signText) -- Warnschild
+ sign.create_sign(135, 99, signText) -- Warnschild
+
+ -- Wachen
+ wache = create_npc("Wache", 7, 96 * TILESIZE + 16, 96 * TILESIZE + 16, wache_talk, nil) --- Wache
+ create_npc("Wache", 7, 106 * TILESIZE + 16, 96 * TILESIZE + 16, wache_talk, nil) --- Wache
+ 
+ -- Trigger für die Überwachung des Torbereiches
+ mana.trigger_create(145 * TILESIZE, 17 * TILESIZE, 5 * TILESIZE, 52 * TILESIZE, "wache_trigger", 1, true) --- Trigger Tor
+ 
 end)
+
+function wache_trigger(ch, id)
+   if (mana.being_type(ch) ~= TYPE_MONSTER) then --- Nur Player durchlassen
+	 local count = mana.chr_inv_count(ch, 40010)
+	 
+	 if count == 0 then
+	   local x = mana.posX(ch)
+	   local y = mana.posY(ch)
+	   mana.chr_warp(ch, mana.get_map_id(), x, 90 * TILESIZE) 
+	   
+	   mana.being_say(wache, invertika.get_random_element("Keinen Schritt weiter!",
+	  "Verlassen sie sofort dieses Gelände!",
+	  "Kein Zutritt!")
+	 else
+	   mana.being_say(wache, invertika.get_random_element("Willkommen Sir.",
+	  "Sir.",
+	  "Keine besonderen Vorkommnise, Sir.")
+	 end
+  end
+ end
+ 
+function wache_talk(npc, ch)
+	do_message(npc, ch, invertika.get_random_element("Verschwinden sie!",
+	    "Kein Zutritt!"))
+	  do_npc_close(npc, ch)
+end
