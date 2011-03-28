a32c52a10001eac1b1d2e24fc61a504ab99f6921
seeseekey <seeseekey@784bf6b6-0765-11df-962c-b5c252e50527>
2011-03-28 17:55:41 +0000

    Netheksäule hinzugefügt.
    git-svn-id: https://invertika.googlecode.com/svn/trunk@4068 784bf6b6-0765-11df-962c-b5c252e50527

--- scripts/maps/ow-p0002-p0005-o0000.lua
+++ scripts/maps/ow-p0002-p0005-o0000.lua
@@ -15,9 +15,11 @@
 ----------------------------------------------------------------------------------
 
 require "scripts/lua/npclib"
+require "scripts/libs/nethek"
 
 dofile("data/scripts/libs/warp.lua")
 
 atinit(function()
  create_inter_map_warp_trigger(19001, 99, 87, 77) --- Intermap warp
+ nethek.create_netheksaeule(167 * TILESIZE, 101 * TILESIZE + 16) --- Netheksäule
 end)
