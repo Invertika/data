e7213904342e1d7085bac53ae6e1073dfcbc412a
seeseekey <seeseekey@784bf6b6-0765-11df-962c-b5c252e50527>
2011-04-06 17:31:33 +0000

    Landschaftsgärtner eingefügt.
    git-svn-id: https://invertika.googlecode.com/svn/trunk@4214 784bf6b6-0765-11df-962c-b5c252e50527

--- scripts/maps/ow-n0002-n0002-o0000.lua
+++ scripts/maps/ow-n0002-n0002-o0000.lua
@@ -5,7 +5,7 @@
 -- anderer Dinge.                                                               --
 --                                                                              --
 ----------------------------------------------------------------------------------
---  Copyright 2008 The Invertika Development Team                               --
+--  Copyright 2008-2011 The Invertika Development Team                          --
 --                                                                              --
 --  This file is part of Invertika.                                             --
 --                                                                              --
@@ -20,4 +20,33 @@ dofile("data/scripts/libs/warp.lua")
 
 atinit(function()
  create_inter_map_warp_trigger(12, 2, 14, 24) --- Intermap warp
+ 
+ create_npc("Peter", 46, 78 * TILESIZE + 16, 42 * TILESIZE + 16, peter_talk, nil) --- Peter
+ create_npc("Hans", 46, 88 * TILESIZE + 16, 43 * TILESIZE + 16, hans_talk, nil) --- Hans
+ create_npc("Siegfried", 46, 101 * TILESIZE + 16, 42 * TILESIZE + 16, siegfried_talk, nil) --- Siegfried
 end)
+
+function peter_talk(npc, ch)
+	do_message(npc, ch, invertika.get_random_element("Gerade Kanten und symmetrisch muss es sein.",
+	  "Wir sorgen dafür das in der Welt von Invertika eine gewisse Gradlinigkeit herscht.",
+	  "Ich bin hier der mit dem 90 Grad Winkelmaß.",
+	  "Akkurat muss es geschnitten sein, Akkurat!",
+	  "Bei uns wird das sehr sehr gewissenhaft gemacht."))
+	do_npc_close(npc, ch)
+end
+
+function hans_talk(npc, ch)
+	do_message(npc, ch, invertika.get_random_element("Wir sind Landschaftsgärtner. Das muss schließlich alles seine Ordnung haben.",
+	  "Da drüben haben wir ein kleines Problem mit dieser Blume. Schrecklich.",
+	  "Jetzt steht diese Blume dort schon seit 5 Tagen und wir haben noch kein Mittel gegen sie gefunden.",
+	  "Das mit der Blume hätte nicht passieren dürfen."))
+	do_npc_close(npc, ch)
+end
+
+function siegfried_talk(npc, ch)
+	do_message(npc, ch, invertika.get_random_element("Wir sorgen für Ordnung und Ordnung und ganz wichtig Ordnung.",
+	  "Verdammt, was macht diese Blume hier?",
+	  "Diese Blume zerstört die Perfektion.",
+	  "Neeeeeeeeeeeeeeiiiiiiiiiiiiinnnnnnnn, diese Blume darf nicht sein.")
+	do_npc_close(npc, ch)
+end
