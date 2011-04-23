7ac827fef8872d91ede15cb8608c375a649ebb2d
erikschilling@freenet.de <erikschilling@freenet.de@784bf6b6-0765-11df-962c-b5c252e50527>
2011-04-23 10:31:04 +0000

    Roststock: Icero: Quest implementiert.
    git-svn-id: https://invertika.googlecode.com/svn/trunk@4331 784bf6b6-0765-11df-962c-b5c252e50527

--- scripts/maps/iw-roststock-flat02-o00-o00-o00.lua
+++ scripts/maps/iw-roststock-flat02-o00-o00-o00.lua
@@ -23,11 +23,77 @@ atinit(function()
 end)
 
 function icero_talk(npc, ch)
-    do_message(npc, ch, invertika.get_random_element("Es hat doch alles keinen Sinn mehr.",
-                                                     "Ich habe keine Lust mehr.",
-                                                     "*Gääääähn*",
-                                                     "Hier ist nix los.",
-                                                     "zZzzzzz. zZzzzzz. Wa? Hast du was gesagt?",
-                                                     "Ich glaube ich geh schlafen..."))
+    local quest_string = "roststock_icero_quest"
+    invertika.init_quest_status(ch, quest_String)
+    local get_qstatus = function() return invertika.get_quest_status(ch, quest_string) end
+    local set_qstatus = function(x) invertika.set_quest_status(ch, quest_string, x) end
+
+    if get_qstatus() == 0 then
+        do_message(npc, ch, invertika.get_random_element("Es hat doch alles keinen Sinn mehr.",
+                                                        "Ich habe keine Lust mehr.",
+                                                        "*Gääääähn*",
+                                                        "Hier ist nix los.",
+                                                        "zZzzzzz. zZzzzzz. Wa? Hast du was gesagt?",
+                                                        "Ich glaube ich geh schlafen..."))
+        set_qstatus(1)
+    elseif get_qstatus() == 1 then
+        do_message(npc, ch, "Inspirin! Inspirin! Bring mir Inspirin!")
+        if mana.chr_inv_count(ch, 30015) > 0 then
+            do_message(npc, ch, "Gib mir dein Inspirin! Bitte!")
+            while true do
+                local v = do_choice(npc, ch, "Hier. Nimm.", "Ich hab keine.")
+                if v == 1 then
+                    invertika.add_items(ch, 30015, "Inspirin", -1)
+                    set_qstatus(2)
+                    do_message(npc, ch, "*schluckt die Tablette hastig*")
+                    do_message(npc, ch, "*atmet tief durch*")
+                    do_message(npc, ch, "Danke.")
+                    break
+                elseif v == 2 then
+                    do_message(npc, ch, "*schaut wild umher*")
+                    break
+                end
+            end
+        end
+    elseif get_qstatus() == 2 then
+        do_message(npc, ch, "Hast du mal ein Bier?")
+        if mana.chr_inv_count(ch, 30006) > 0 then
+            while true do
+                local v = do_choice(npc, ch, "Ihm ein Bier geben.", "Das Bier behalten.")
+                if v == 1 then
+                    invertika.add_items(ch, 30006, -1, "Bier")
+                    do_message(npc, ch, "Danke. Hier. Ich hab noch ein paar alte Tabletten. Ich teste die.")
+                    invertika.add_items(ch, 30012, 1, "Inspirin Megaplus")
+                    set_qstatus(3)
+                    break
+                elseif v == 2 then
+                    break
+                end
+            end
+        end
+    elseif get_qstatus() == 3 then
+        do_message(npc, ch, "Im Moment fühle ich mich besser.")
+        do_message(npc, ch, "Ich muss dringend mal wieder zum Psychiater.")
+        while true do
+            local v = do_choice(npc, ch, "Soll ich einen für dich suchen?", "Auf Wiedersehen.")
+            if v == 1 then
+                do_message(npc, ch, "Ich war mal bei einem in Selphi Timlet. Wenn du herausfindest wer das war wäre ich glücklich.")
+                set_qstatus(4)
+                break
+            elseif v == 2 then
+                break
+            end
+        end
+    elseif get_qstatus() == 4 then
+        do_message(npc, ch, "Der Psychiater wohnte in Selphi Timlet. Kannst du ihn suchen?")
+    elseif get_qstatus() == 5 then
+        do_message(npc, ch, "Ich habe schon gehört. Ceech will mal vorbei kommen.")
+        do_message(npc, ch, "Danke für deine Hilfe!")
+        set_qstatus(6)
+    elseif get_qstatus() == 6 then
+        do_message(npc, ch, invertika.get_random_element("Es ist hart sich wieder an das Leben zu gewöhnen.",
+          "Ab und zu nehme ich noch eine Tablette.",
+          "Ich versuche mich zu bessern."))
+    end
     do_npc_close(npc, ch)
 end
