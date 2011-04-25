b13686b12b2b717e0fbb30782d826b5963cd99ba
seeseekey <seeseekey@784bf6b6-0765-11df-962c-b5c252e50527>
2011-04-25 18:45:43 +0000

    Debugmeldungen modifiziert.
    git-svn-id: https://invertika.googlecode.com/svn/trunk@4394 784bf6b6-0765-11df-962c-b5c252e50527

--- scripts/libs/invertika.lua
+++ scripts/libs/invertika.lua
@@ -61,9 +61,9 @@ end
 -- @param questname Der Name des Quests.
 -- @return Aktuellen Status des Quests.
 function get_quest_status(ch, questname)
-    mana.chatmessage(ch, "Debug: Char" + ch)
-    mana.chatmessage(ch, "Debug: Questname" + questname)
-    mana.chatmessage(ch, "Debug: " + get_quest_var(ch, questname))
+    mana.chatmessage(ch, "Debug: Char" .. tostring(ch))
+    mana.chatmessage(ch, "Debug: Questname" .. tostring(questname))
+    mana.chatmessage(ch, "Debug: " .. tostring(get_quest_var(ch, questname)))
     return tonumber(get_quest_var(ch, questname))
 end
 
