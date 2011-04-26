3cb6e353d15ff1adde314bdf828d61a3e7a2bbac
seeseekey <seeseekey@784bf6b6-0765-11df-962c-b5c252e50527>
2011-04-26 09:34:19 +0000

    Debugmeldungen weiter modifiziert.
    git-svn-id: https://invertika.googlecode.com/svn/trunk@4397 784bf6b6-0765-11df-962c-b5c252e50527

--- scripts/maps/uw-vacare-o0000-o0000-o0000.lua
+++ scripts/maps/uw-vacare-o0000-o0000-o0000.lua
@@ -143,7 +143,7 @@ function kolbert_talk(npc, ch)
     mana.chatmessage(ch, "Debug: invertika.init_quest_status ... OK")
     
     if invertika.get_quest_status(ch, invertika.get_quest_status(ch, quest_string) == 0) then
-      mana.chatmessage(ch, "Debug: invertika.get_quest_status ... OK")
+      mana.chatmessage(ch, "Debug: vacare_first_weapon ... OK")
       
       do_message(npc, ch, "Du willst kämpfen? Nun gut zum Kämpfen benötigst du erst einmal eine Waffe.")
       
