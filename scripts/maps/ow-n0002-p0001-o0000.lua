fbbf1835f03f521e97497169b2412bd9e4ef2102
chrnicolai@gmail.com <chrnicolai@gmail.com@784bf6b6-0765-11df-962c-b5c252e50527>
2011-05-14 15:29:11 +0000

    Ortseingangsschilder Qakari erstellt.
    git-svn-id: https://invertika.googlecode.com/svn/trunk@4498 784bf6b6-0765-11df-962c-b5c252e50527

--- scripts/maps/ow-n0002-p0001-o0000.lua
+++ scripts/maps/ow-n0002-p0001-o0000.lua
@@ -16,24 +16,28 @@
 
 require "scripts/lua/npclib"
 require "scripts/libs/nethek"
+require "scripts/libs/sign"
 
 dofile("data/scripts/libs/warp.lua")
 
 atinit(function()
- create_inter_map_warp_trigger(19, 7, 17, 29) --- Intermap warp
- nethek.create_netheksaeule(161 * TILESIZE, 25 * TILESIZE + 16) --- Netheksäule
-  
- create_npc("Gabriel", 46, 59 * TILESIZE + 16, 160 * TILESIZE + 16, gabriel_talk, nil) --- Gabriel
- create_npc("Patrick", 46, 59 * TILESIZE + 16, 160 * TILESIZE + 16, patrick_talk, nil) --- kevin
+    create_inter_map_warp_trigger(19, 7, 17, 29) --- Intermap warp
+    nethek.create_netheksaeule(161 * TILESIZE, 25 * TILESIZE + 16) --- Netheksäule
+
+    sign_entrance = "Qakari"
+    sign.create_sign(83, 188, sign_entrance) --- Schild Ortseingang
+    sign.create_sign(95, 188, sign_entrance) --- Schild Ortseingang
+
+    create_npc("Gabriel", 46, 59 * TILESIZE + 16, 160 * TILESIZE + 16, gabriel_talk, nil) --- Gabriel
+    create_npc("Patrick", 46, 59 * TILESIZE + 16, 160 * TILESIZE + 16, patrick_talk, nil) --- kevin
 end)
 
 function gabriel_talk(npc, ch)
-	do_message(npc, ch, "Ich bin Totengräber und habe schon lange keine neuen Aufträge mehr bekommen. Warum mussten auch die Untoten über die Stadt kommen. Mir tun sie komischerweise nichts. Villeicht, weil ich Ihre Gräber schon seit langem Pflege. Manchmal habe ich das gefühl, sie wollen mit mir sprechen, aber wenn ich zu ihnen geheund versuche ihnen zuzuhören, stammeln sie nur unverständliches Gemurmel.")
-	do_npc_close(npc, ch)
+    do_message(npc, ch, "Ich bin Totengräber und habe schon lange keine neuen Aufträge mehr bekommen. Warum mussten auch die Untoten über die Stadt kommen. Mir tun sie komischerweise nichts. Villeicht, weil ich Ihre Gräber schon seit langem Pflege. Manchmal habe ich das gefühl, sie wollen mit mir sprechen, aber wenn ich zu ihnen geheund versuche ihnen zuzuhören, stammeln sie nur unverständliches Gemurmel.")
+    do_npc_close(npc, ch)
 end
 
 function patrick_talk(npc, ch)
-
-	do_message(npc, ch, "Hallo Fremder, sei vorsichtig in der Umgebung dieser Stadt. Die Untoten reagieren Fremden gegenüber nicht sehr freundlich. Solltest du den Mut haben in die Stadt zu gehen, sei noch vorsichtiger. Auch wenn sie schon gestorben sind, haben sie etwas dagegen, wenn die Gräber von unbekannten Personen besucht werden.")
-	do_npc_close(npc, ch)
+    do_message(npc, ch, "Hallo Fremder, sei vorsichtig in der Umgebung dieser Stadt. Die Untoten reagieren Fremden gegenüber nicht sehr freundlich. Solltest du den Mut haben in die Stadt zu gehen, sei noch vorsichtiger. Auch wenn sie schon gestorben sind, haben sie etwas dagegen, wenn die Gräber von unbekannten Personen besucht werden.")
+    do_npc_close(npc, ch)
 end
