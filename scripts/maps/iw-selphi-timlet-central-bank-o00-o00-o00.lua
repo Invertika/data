c17a1696d89244791e728946b20b5c0a4663ca91
seeseekey <seeseekey@784bf6b6-0765-11df-962c-b5c252e50527>
2011-05-14 14:12:39 +0000

    Position der Wachen korrigiert.
    git-svn-id: https://invertika.googlecode.com/svn/trunk@4484 784bf6b6-0765-11df-962c-b5c252e50527

--- scripts/maps/iw-selphi-timlet-central-bank-o00-o00-o00.lua
+++ scripts/maps/iw-selphi-timlet-central-bank-o00-o00-o00.lua
@@ -5,7 +5,7 @@
 -- anderer Dinge.                                                               --
 --                                                                              --
 ----------------------------------------------------------------------------------
---  Copyright 2008 The Invertika Development Team                               --
+--  Copyright 2008-2011 The Invertika Development Team                          --
 --                                                                              --
 --  This file is part of Invertika.                                             --
 --                                                                              --
@@ -26,13 +26,13 @@ atinit(function()
  create_npc("Banker", 11, 75 * TILESIZE + 16, 85 * TILESIZE + 16, banker.banker_talk, nil) --- Banker
  
  --npcs
- wache = create_npc("Wache", 29, 30 * TILESIZE + 16, 86 * TILESIZE + 16, wache_talk, nil) --- Wache
- create_npc("Wache", 29, 36 * TILESIZE + 16, 86 * TILESIZE + 16, wache_talk, nil) --- Wache
+ wache = create_npc("Wache", 29, 30 * TILESIZE + 16, 87 * TILESIZE + 16, wache_talk, nil) --- Wache
+ create_npc("Wache", 29, 36 * TILESIZE + 16, 87 * TILESIZE + 16, wache_talk, nil) --- Wache
  
  create_npc("Mertox", 2, 24 * TILESIZE + 16, 24 * TILESIZE + 16, mertox_talk, nil) --- Mertox
  
   -- Trigger für die Überwachung des Bereiches
- mana.trigger_create(30 * TILESIZE, 85 * TILESIZE, 7 * TILESIZE, 3 * TILESIZE, "wache_trigger", 1, true) --- Trigger
+ mana.trigger_create(30 * TILESIZE, 84 * TILESIZE, 7 * TILESIZE, 3 * TILESIZE, "wache_trigger", 1, true) --- Trigger
 end)
 
 function wache_trigger(ch, id)
