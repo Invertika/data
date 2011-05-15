63b5304d3ae9a8c2537e6055662fe88c9649dcd7
chrnicolai@gmail.com <chrnicolai@gmail.com@784bf6b6-0765-11df-962c-b5c252e50527>
2011-05-15 08:35:53 +0000

    Ortseingangsschilder Narva erstellt
    git-svn-id: https://invertika.googlecode.com/svn/trunk@4525 784bf6b6-0765-11df-962c-b5c252e50527

--- scripts/maps/ow-p0003-p0002-o0000.lua
+++ scripts/maps/ow-p0003-p0002-o0000.lua
@@ -15,9 +15,15 @@
 ----------------------------------------------------------------------------------
 
 require "scripts/lua/npclib"
+require "scripts/libs/sign"
 
 dofile("data/scripts/libs/warp.lua")
 
 atinit(function()
- create_inter_map_warp_trigger(97, 107, 95, 85) --- Intermap warp
+    create_inter_map_warp_trigger(97, 107, 95, 85) --- Intermap warp
+
+    sign_entrance = "Narva"
+    sign.create_sign(154, 128, sign_entrance) --- Schild Ortseingang
+    sign.create_sign(62, 106, sign_entrance) --- Schild Ortseingang
+    sign.create_sign(95, 47, sign_entrance) --- Schild Ortseingang
 end)
