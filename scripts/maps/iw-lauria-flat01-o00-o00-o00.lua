70c6940926a33b6995010b7c7a2addfe2fa16117
ablu.erikschilling@gmail.com <ablu.erikschilling@gmail.com@784bf6b6-0765-11df-962c-b5c252e50527>
2011-05-21 20:04:23 +0000

    Lauria: flat01: neu gemappt.
    git-svn-id: https://invertika.googlecode.com/svn/trunk@4587 784bf6b6-0765-11df-962c-b5c252e50527

--- scripts/maps/iw-lauria-flat01-o00-o00-o00.lua
+++ scripts/maps/iw-lauria-flat01-o00-o00-o00.lua
@@ -15,9 +15,17 @@
 ----------------------------------------------------------------------------------
 
 require "scripts/lua/npclib"
+require "scripts/libs/invertika"
 
 dofile("data/scripts/libs/warp.lua")
 
 atinit(function()
-    
+    create_npc("Ansdan", 60, 25 * TILESIZE + 16, 39 * TILESIZE + 16, ansdan_talk, nil) --- Ansdan
 end)
+
+function andsdan_talk(npc, ch)
+    do_message(npc, ch, invertika.get_random_element("Willkommen!",
+      "Dies ist mein Bescheidenes Heim.",
+      "Möge Invertika euch beschützen."))
+    do_npc_close(npc, ch)
+end
