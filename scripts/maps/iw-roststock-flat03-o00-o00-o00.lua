e36977456d311f40af068c4a76266ce4c03afd65
ablu.erikschilling@gmail.com <ablu.erikschilling@gmail.com@784bf6b6-0765-11df-962c-b5c252e50527>
2011-05-24 19:22:17 +0000

    Quest gebaut.
    git-svn-id: https://invertika.googlecode.com/svn/trunk@4618 784bf6b6-0765-11df-962c-b5c252e50527

--- scripts/maps/iw-roststock-flat03-o00-o00-o00.lua
+++ scripts/maps/iw-roststock-flat03-o00-o00-o00.lua
@@ -22,11 +22,41 @@ atinit(function()
 end)
 
 function marle_talk(npc, ch)
-	do_message(npc, ch, invertika.get_random_element("Ich würde gerne mal Effendi besuchen.",
-	  "Die beste Freundin von Effendi ist Anne. Habe ich zumindestens gehört.",
-	  "Eigentlich heißt er garnicht Effendi sondern Effendi der Erste.",
-	  "Du hättest dir ruhig die Schuhe abputzen können. Jetzt ist hier alles voller Sand.",
-	  "In den westlichen Gegenden soll es manchmal sogar Schnee geben habe ich gehört.",
-	  "Früher sammelte ich Porzellanfiguren.")) 
+    local quest_string = "roststock_marle_quest"
+    invertika.init_quest_status(ch, quest_string)
+    local get_qstatus = function() return invertika.get_quest_status(ch, quest_string) end
+    local set_qstatus = function(x) invertika.set_quest_status(ch, quest_string, x) end
+    if get_qstatus() == 0 then
+        local count = mana.chr_inv_count(ch, 40037)
+        if count > 0 then
+            do_message(npc, ch, "Porzellan! Ich sehe du hast eine Teekanne aus Porzellan.")
+            do_message(npc, ch, "Diese Kanne hat unschätzbaren Wert!")
+            do_message(npc, ch, "Gibst du mir diese Koststück?")
+            while true do
+                local v = do_choice(npc, ch, "Ja.", "Nein.")
+                if v == 1 then
+                    invertika.add_items(ch, 40037, -count, "Teekanne aus Porzellan")
+                    invertika.add_items(ch, 40028, count, "antike Münze")
+                    do_message(npc, ch, "Vielen Dank! Nimm dies als Dank.")
+                    break
+                else
+                    do_message(npc, ch, "Schade.")
+                    do_message(npc, ch, "Bedenke, dass niemand außer mir den Wert dieser Kanne erkennt.")
+                    break
+                end
+            end
+        else
+            do_message(npc, ch, invertika.get_random_element("Ich sammele gerne Porzellan.",
+              "Willst du mal meine Porzellan Sammlung sehen?",
+              "Wenn du altes Porzellan findest. Bringe es mir."))
+        end
+    else
+	    do_message(npc, ch, invertika.get_random_element("Ich würde gerne mal Effendi besuchen.",
+	      "Die beste Freundin von Effendi ist Anne. Habe ich zumindestens gehört.",
+	      "Eigentlich heißt er garnicht Effendi sondern Effendi der Erste.",
+	      "Du hättest dir ruhig die Schuhe abputzen können. Jetzt ist hier alles voller Sand.",
+	      "In den westlichen Gegenden soll es manchmal sogar Schnee geben habe ich gehört.",
+	      "Früher sammelte ich Porzellanfiguren.")) 
+    end
 	 do_npc_close(npc, ch)
 end
