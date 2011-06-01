1ccd29e2893529346c8f9fdc911abe0f428860fb
seeseekey <seeseekey@784bf6b6-0765-11df-962c-b5c252e50527>
2011-06-01 14:00:34 +0000

    Weitere Korrekturen.
    git-svn-id: https://invertika.googlecode.com/svn/trunk@4647 784bf6b6-0765-11df-962c-b5c252e50527

--- scripts/maps/iw-desert-cruel-truths-of-life-house-o00-o00-o00.lua
+++ scripts/maps/iw-desert-cruel-truths-of-life-house-o00-o00-o00.lua
@@ -32,28 +32,34 @@ function veritas_talk(npc, ch)
 				    local vMoney = do_choice(npc, ch, "Ja.", "Nein.")
 					
 					if vMoney == 1 then
-					    mana.chr_money(ch -15000)
+					    local PlayerMoney=mana.chr_money(ch)
+		                if PlayerMoney >= 15000 then
+					        invertika.set_money(ch -15000)
 						
-					    do_message(npc, ch, "Ausgezeichnet. Ich hoffe du bist vorbereitet. Ich lese nun eine grausame Wahrheit des Lebens aus diesem Buch.")
+					        do_message(npc, ch, "Ausgezeichnet. Ich hoffe du bist vorbereitet. Ich lese nun eine grausame Wahrheit des Lebens aus diesem Buch.")
 						
-						mana.effect_create(5, 37 * TILESIZE, 19 * TILESIZE);
-						mana.effect_create(5, 44 * TILESIZE, 19 * TILESIZE);
+						    mana.effect_create(5, 37 * TILESIZE, 19 * TILESIZE);
+						    mana.effect_create(5, 44 * TILESIZE, 19 * TILESIZE);
 						
-					    do_message(npc, ch, invertika.get_random_element("Es gibt keinen Gott!", "Genieße dein Leben, denn nach dem Tod ist es vorbeit!", "Nach dem Tod kommt nur noch Dunkelheit!", "Alles ist vergänglich!",
-						                                                 "Jeder vergangene Moment könnte der letzte sein!", "Der Tod ist nur einen Fingerzeig entfernt!", "Moral ist nichts weiter als das klammern an ein universelles Prinzip!",
-																		 "Nichts wird so sein wie es war!"))
+					        do_message(npc, ch, invertika.get_random_element("Es gibt keinen Gott!", "Genieße dein Leben, denn nach dem Tod ist es vorbeit!", "Nach dem Tod kommt nur noch Dunkelheit!", "Alles ist vergänglich!",
+						                                                     "Jeder vergangene Moment könnte der letzte sein!", "Der Tod ist nur einen Fingerzeig entfernt!", "Moral ist nichts weiter als das klammern an ein universelles Prinzip!",
+									    									 "Nichts wird so sein wie es war!"))
 																		 
-						mana.effect_create(6, 37 * TILESIZE, 19 * TILESIZE);
-						mana.effect_create(6, 44 * TILESIZE, 19 * TILESIZE);
+						    mana.effect_create(6, 37 * TILESIZE, 19 * TILESIZE);
+						    mana.effect_create(6, 44 * TILESIZE, 19 * TILESIZE);
 						
-						mana.monster_create(31, 36 * TILESIZE, 22 * TILESIZE);
-						mana.monster_create(31, 45 * TILESIZE, 22 * TILESIZE);
+						    mana.monster_create(31, 36 * TILESIZE, 22 * TILESIZE);
+						    mana.monster_create(31, 45 * TILESIZE, 22 * TILESIZE);
 						
-						mana.monster_create(31, 39 * TILESIZE, 18 * TILESIZE);
-						mana.monster_create(31, 42 * TILESIZE, 18 * TILESIZE);
+						    mana.monster_create(31, 39 * TILESIZE, 18 * TILESIZE);
+						    mana.monster_create(31, 42 * TILESIZE, 18 * TILESIZE);
 						
-						mana.monster_create(31, 39 * TILESIZE, 29 * TILESIZE);
-						mana.monster_create(31, 42 * TILESIZE, 29 * TILESIZE);
+						    mana.monster_create(31, 39 * TILESIZE, 29 * TILESIZE);
+						    mana.monster_create(31, 42 * TILESIZE, 29 * TILESIZE);
+						else
+						    do_message(npc, ch, "Du solltest im Besitz der entsprechenden Geldmenge sein. So wirst du die Wahrheit nicht erfahren.")
+						end
+						
 						break;
 					elseif vMoney == 2 then
 					    do_message(npc, ch, invertika.get_random_element("Gut dann gehe deinen Weg...", "Vielleicht ist es besser so...", "So sei es..."))
