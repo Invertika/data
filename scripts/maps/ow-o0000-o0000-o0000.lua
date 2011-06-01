097683e34d94ef01ace48ce7e42d3e37b2689418
seeseekey <seeseekey@784bf6b6-0765-11df-962c-b5c252e50527>
2011-06-01 14:21:42 +0000

    Typo korrigiert.
    git-svn-id: https://invertika.googlecode.com/svn/trunk@4649 784bf6b6-0765-11df-962c-b5c252e50527

--- scripts/maps/ow-o0000-o0000-o0000.lua
+++ scripts/maps/ow-o0000-o0000-o0000.lua
@@ -1,7 +1,7 @@
 -- Externe Map Skripting Datei
--- In dieser Datei stehen die entsprechenden externen NPC's, Trigger und anderer Dinge.
+-- In dieser Datei stehen die entsprechenden externen NPCs, Trigger und anderer Dinge.
 --
--- Copyright 2008-2011 The Invertika Development Team
+-- © 2008-2011 by The Invertika Development Team
 --
 -- This file is part of Invertika. Invertika is free software; you can redistribute 
 -- it and/or modify it under the terms of the GNU General  Public License as published 
