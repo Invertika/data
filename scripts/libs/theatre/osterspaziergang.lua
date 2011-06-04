fbc9d969e2ecf199023cf92410b3478dc931dd1f
ablu.erikschilling@gmail.com <ablu.erikschilling@gmail.com@784bf6b6-0765-11df-962c-b5c252e50527>
2011-06-04 14:22:48 +0000

    Theatre-lib: Fehler korrigiert.
    git-svn-id: https://invertika.googlecode.com/svn/trunk@4705 784bf6b6-0765-11df-962c-b5c252e50527

--- scripts/libs/theatre/osterspaziergang.lua
+++ scripts/libs/theatre/osterspaziergang.lua
@@ -6,6 +6,11 @@ plotOsterspaziergang.step=0
 --NPCs
 plotOsterspaziergang.Julia=0
 
+-- Talkfunktion
+plotOsterspaziergang.npc_talk = function(npc, ch)
+  mana.chatmessage(ch, "Schauspieler reden nicht mit dem Publikum.")
+end
+
 -- Konstuktor
 function plotOsterspaziergang:new()
 	local res = {}
@@ -15,12 +20,11 @@ function plotOsterspaziergang:new()
 end
 
 function plotOsterspaziergang:npc_talk(npc, ch)
-  mana.chatmessage(ch, "Schauspieler reden nicht mit dem Publikum.")
 end
 
 function plotOsterspaziergang:nextPlotStep()
   if self.step == 0 then
-    self.Julia=create_npc("Julia", 45, 46 * TILESIZE + 16, 17 * TILESIZE + 16, self:npc_talk, nil) --- Julia
+    self.Julia=create_npc("Julia", 45, 46 * TILESIZE + 16, 17 * TILESIZE + 16, self.npc_talk, nil) --- Julia
     mana.being_say(self.Julia, "Vom Eise befreit sind Strom und Bäche")
   elseif self.step == 1 then
     mana.being_say(self.Julia, "Durch des Frühlings holden, belebenden Blick,")
