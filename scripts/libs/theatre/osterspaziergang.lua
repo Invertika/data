5c1738182e92f98e2c00200b244f0103fa56b8e2
seeseekey <seeseekey@784bf6b6-0765-11df-962c-b5c252e50527>
2011-06-04 13:29:58 +0000

    Weitere Korrekturen.
    git-svn-id: https://invertika.googlecode.com/svn/trunk@4696 784bf6b6-0765-11df-962c-b5c252e50527

--- scripts/libs/theatre/osterspaziergang.lua
+++ scripts/libs/theatre/osterspaziergang.lua
@@ -19,14 +19,14 @@ function plotOsterspaziergang.npc_talk(npc, ch)
 end
 
 function plotOsterspaziergang:nextPlotStep()
-  if self.step==0 then
-    self.Julia=create_npc("Julia", 45, 46 * TILESIZE + 16, 17 * TILESIZE + 16, plotOsterspaziergang.npc_talk, nil) --- Julia
+  if plotOsterspaziergang.step==0 then
+    plotOsterspaziergang.Julia=create_npc("Julia", 45, 46 * TILESIZE + 16, 17 * TILESIZE + 16, plotOsterspaziergang.npc_talk, nil) --- Julia
     mana.being_say(self.Julia, "Vom Eise befreit sind Strom und Bäche")
-  elseif self.step==1 then
+  elseif plotOsterspaziergang.step==1 then
     mana.being_say(self.Julia, "Durch des Frühlings holden, belebenden Blick,")
-  elseif self.step==2 then
+  elseif plotOsterspaziergang.step==2 then
     mana.being_say(self.Julia, "Im Tale grünet Hoffnungsglück;")
   end
   
-  self.step=self.step+1
+  plotOsterspaziergang.step=plotOsterspaziergang.step+1
 end
