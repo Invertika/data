53e4afaa85ce29b8dfc6dc64a58d099e957034c8
ablu.erikschilling@gmail.com <ablu.erikschilling@gmail.com@784bf6b6-0765-11df-962c-b5c252e50527>
2011-06-25 13:20:03 +0000

    Lib für Postboten implementiert. Idee: Issue 294. Ein Postmann kann erstellt werden indem man create_postman_npc(id, name, sprite, x, y, possible_designations, itemid) aufruft. - id ist die id die in den Quest Variablen genutzt wird. - name ist der Name der Poststation der in den Dialogen verwendet wird. - possible_designations gibt die möglichen Lieferziele an:   - Beispiel: possible_designations = {{id="id1", distance=10}, {id="id2", distance=5}}   - id ist die Id des Ziels, distance wird dafür verwendet um größere Belohnungen bei weiteren Reisen zu verteilen. Als Belohnung werden immer genau distance Items gegeben. - itemid is die Id des Items welches das Paket für diese Poststation verkörpert.
    TODO:
    - Testen
    - Belohnungen einstellen
    - NPCs verteilen
    
    
    git-svn-id: https://invertika.googlecode.com/svn/trunk@4744 784bf6b6-0765-11df-962c-b5c252e50527

---
+++ scripts/libs/postmen.lua
@@ -0,0 +1,152 @@
+-- Modul Postmen
+
+module("postmen", package.seeall)
+
+require "scripts/lua/npclib"
+require "scripts/libs/invertika"
+
+postman = {}
+
+function create_postemen_npc(id, name, sprite, x, y, possible_designations, itemid)
+    new_postman = create_npc("Postmann", sprite, x, y, postman_talk, nil)
+    postman[new_postman] = {id=id, name=name, designations=possible_designations, item=itemid}
+end
+
+--- Prüft was für einen Auftrag der Spieler von dem NPC hat.
+-- @param npc Der Postmann NPC
+-- @param ch Der Spieler
+-- @return Das Ziel, dass von dem npc vergeben wurde.
+function get_order_designation(npc, ch)
+    return invertika.get_quest_status_string(ch, string.format("%s_order_designation", postman[npc].id))
+end
+
+--- Gibt das Ziel eines Auftrag der durch den Postboten mit einer id vergeben wurde
+-- @param ch Der Spieler
+-- @param id Id des Auftraggebers
+-- @return Id des Ziels das durch den Auftraggeber id vergeben wurde
+function get_designation_by_startid(ch, id)
+    return invertika.get_quest_status_string(ch, string.format("%s_order_designation", id))
+end
+
+--- Prüft ob bei dem NPC ein Auftrag erfüllt werden kann.
+-- @param npc Der Postmann NPC
+-- @param ch Der Spieler
+-- @return (true, npc) wenn ein Auftrag erfüllt werden kann, false wenn nicht.
+function can_do_order(npc, ch)
+    for i, v in ipairs(postman) do
+        if get_order_designation(npc, ch) == postman[npc].id then
+            return true, i
+        end
+    end
+    return false
+end
+
+--- Prüft ob ein Paket für den NPC im Inventar ist.
+-- @param npc Der Postmann NPC
+-- @param ch Der Spieler
+-- @return true wenn ja
+function has_package_for_npc(npc, ch)
+    return mana.chr_inv_count(ch, postman[npc].item) > 0
+end
+
+--- Sucht ein neues Ziel aus.
+-- @param npc Der Postmann NPC
+-- @param ch Der Spieler
+-- @return Id des neuen Postbotens
+function get_new_designation(npc)
+    return invertika.get_random_element(postman[npc].designations).id
+end
+
+--- Gibt den Namen für die Id zurück.
+-- @param id Die Id.
+-- @return den lesbaren Namen für die Id
+function get_name_by_id(id)
+    for i, v in ipairs(postman) do
+        if v.id == id then
+            return v.name
+        end
+    end
+    error("postmen-lib: get_name_by_id called with not used id")
+end
+
+
+--- Gibt die Distanz vom Start zum Ziel zurück.
+-- @param ch Der Spieler
+-- @param npc_start Auftraggeber NPC
+-- @return Distanz vom Start des Spielers zum Ziel
+function get_distance(ch, npc_start)
+    local designation = get_order_designation(npc, ch)
+    for i, v in ipairs(postman[npc_start].designations) do
+        if v.id == designation then
+            return v.distance
+        end
+    end
+    return nil
+end
+
+--- Gibt die ItemId zurück die von dem Ziel erwartet wird.
+-- @param designation Die Id des Ziels
+-- @return ItemId die vom Ziel erwartet wird
+function get_desired_itemid(designation)
+    for i, v in ipairs(postman) do
+        if v.id = designation then
+            return v.item
+        end
+    end
+end
+
+--- Talk Funktion
+function postman_talk(npc, ch)
+    -- Quest-Stati initialisieren
+    invertika.init_quest_status_string(ch, string.format("%s_order_designation", postman[npc].id))
+    invertika.init_quest_status(ch, string.format("%s_order_done", postman[npc].id))
+    
+    local designation = get_order_designation(npc, ch)
+    
+    if has_package_for_npc(npc, ch) then
+        local has_order, start_npc = can_do_order(npc, ch)
+        if has_order then
+            do_message(npc, ch, invertika.get_random_element("Schon wieder ein Paket für mich? Gib mal her!",
+              string.format("Ein Paket von der Poststelle %s... Das kommt auf diesen Stapel.", postman[start_npc].name)))
+            do_message(npc, ch, "Paket abgeben?")
+            while true do
+                local v = do_choice(npc, ch, "Ja.", "Nein.")
+                if v == 1 then
+                    invertika.add_items(ch, postman[npc].item, -1, string.format("Paket für Poststelle %s", postman[npc].name))
+                    invertika.set_quest_status(ch, string.format("%s_order_done", postman[start_npc].id), 1)
+                    do_message(npc, ch, "Danke.")
+                    break
+                elseif v == 2 then
+                    break
+                end
+            end
+        end
+    elseif designation ~= "" then
+        if invertika.get_quest_status(ch, string.format("%s_order_done")) == 0 then
+            do_message(npc, ch, string.format("Du musst noch das Paket bei der Poststelle %s abgeben", get_name_by_id(designation)))
+        else
+            invertika.set_quest_status(ch, string.format("%s_order_done"), 0)
+            do_message(npc, ch, "Danke, dass du das Paket da abgegeben hast.")
+            local gift = invertika.get_random_element(GIFTS)
+            local number = get_distance(ch, npc)
+            do_message(npc, ch, "Nimm dies als Dank.")
+            invertika.add_items(ch, gift.id, number, gift.name)
+        end
+    else
+        local order_designation = get_new_designation(npc)
+        do_message(npc, ch, string.format("Würdest du für mich ein Paket zur Poststelle %s bringen?", get_name_by_id(order_designation)))
+        while true do
+            local v = do_choice(npc, ch, "Ja.", "Nein.")
+            if v == 1 then
+                invertika.set_quest_status_string(ch, string.format("%s_order_designation", postman[npc].id), order_designation)
+                invertika.add_items(ch, get_desired_itemid(order_designation), 1, string.format("Paket für Poststelle %s", get_name_by_id(order_designation)))
+                do_message(npc, ch, "Danke.")
+                break
+            elseif v == 2 then
+                break
+            end
+        end
+    end
+    do_npc_close(npc, ch)
+end
+
