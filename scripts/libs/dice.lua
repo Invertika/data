270e10d7dfd75ef4cce67e019f39e184917bdd02
ablu.erikschilling@gmail.com <ablu.erikschilling@gmail.com@784bf6b6-0765-11df-962c-b5c252e50527>
2011-07-03 09:14:11 +0000

    Bibliothek und Itemskript für Würfel eingefügt.
    git-svn-id: https://invertika.googlecode.com/svn/trunk@4815 784bf6b6-0765-11df-962c-b5c252e50527

---
+++ scripts/libs/dice.lua
@@ -0,0 +1,41 @@
+----------------------------------------------------------------------------------
+-- Würfel Skript                                                                --
+----------------------------------------------------------------------------------
+--  Copyright 2009 The Invertika Development Team                               --
+--                                                                              --
+--  This file is part of Invertika.                                             --
+--                                                                              --
+--  Invertika is free software; you can redistribute it and/or modify it        --
+--  under the terms of the GNU General  Public License as published by the Free --
+--  Software Foundation; either version 2 of the License, or any later version. --
+----------------------------------------------------------------------------------
+
+module("dice", package.seeall)
+
+require "scripts/lua/npclib"
+
+-- Konstanten
+DICE_ID = 40042
+DICE_NUMBER_OF_SIDES = 6
+
+function dicemanager_talk(npc, ch)
+    if mana.chr_inv_count(ch, DICE_ID) < 1 then
+        do_message(npc, ch, "Du benötigst einen Würfel um mitzuspielen.")
+        do_npc_close(npc, ch)
+        return
+    end
+    while true do
+        local v = do_choice(npc, ch, "Würfeln.", "Tschüss.")
+        if v == 1 then
+            mana.being_say(npc, string.format("%s würfelt %d Augen", mana.being_get_name(ch), get_new_dice_value()))
+            break
+        elseif v == 2 then
+            break
+        end
+    end
+    do_npc_close(npc, ch)
+end
+
+function get_new_dice_value()
+    return math.random(1, DICE_NUMBER_OF_SIDES)
+end
