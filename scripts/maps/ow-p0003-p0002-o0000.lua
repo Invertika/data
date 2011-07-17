161a224a64dc1844e37567a9dd09e152c0a9f4a3
ablu.erikschilling@gmail.com <ablu.erikschilling@gmail.com@784bf6b6-0765-11df-962c-b5c252e50527>
2011-07-17 16:36:39 +0000

    Narva: Marktschreier eingefügt.
    git-svn-id: https://invertika.googlecode.com/svn/trunk@4974 784bf6b6-0765-11df-962c-b5c252e50527

--- scripts/maps/ow-p0003-p0002-o0000.lua
+++ scripts/maps/ow-p0003-p0002-o0000.lua
@@ -29,6 +29,7 @@ atinit(function()
     sign.create_sign(95, 47, sign_entrance) --- Schild Ortseingang
 
     create_npc("Awond", 120, 159 * TILESIZE, 40 * TILESIZE, awond_talk, nil)
+    create_npc("Mordyno", 5, 101 * TILESIZE + 16, 109 * TILESIZE + 16, mordyno_talk, mordyno_update)
 end)
 
 function awond_talk(npc, ch)
@@ -37,4 +38,39 @@ function awond_talk(npc, ch)
       "Ich hoffe meine Frau macht schonmal das Abendessen.",
       "Setz dich doch zu mir."))
     do_npc_close(npc, ch)
+end
+
+function mordyno_talk(npc, ch)
+    mana.npc_trade(npc, ch, false, {
+      {10001, 30, 50},
+      {10013, 30, 140},
+      {10002, 30, 1000},
+      {10009, 30, 500},
+      {20001, 30, 250},
+      {20002, 30, 250},
+      {20011, 30, 250},
+      {20012, 30, 250},
+      {20013, 30, 250},
+      {20014, 30, 250},
+      {20015, 30, 250},
+      {20016, 30, 250},
+      {20017, 30, 250},
+      {20018, 30, 250},
+      {20009, 30, 500} }
+    do_npc_close(npc, ch)
+end
+
+function mordyno_update(npc)
+    npclib.walkaround_wide(npc)
+    if not mordyno_timer then
+        mordyno_timer = 1
+    else
+        mordyno_timer = mordyno_timer + 1
+    end
+    if mordyno_timer == 25 then
+        mana.being_say(npc, invertika.get_random_element("Kauft Leute! Kauf!", 
+          "Hier gibt es nur beste Ware!",
+          "Kommen sie meine Herren, Kommen sie meine Damen!"))
+          mordyno_timer = 0
+    end
 end
