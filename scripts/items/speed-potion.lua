c0cceed3b3a714e1e41b8eedd7669bc4d4105a92
ablu.erikschilling@gmail.com <ablu.erikschilling@gmail.com@784bf6b6-0765-11df-962c-b5c252e50527>
2011-07-29 08:31:16 +0000

    Speedskript und Status gelöscht, da der neue Server eine bessere Möglichkeit bietet und der aktuelle Server ohnehin noch nicht mit Stati klar kommt.
    git-svn-id: https://invertika.googlecode.com/svn/trunk@5105 784bf6b6-0765-11df-962c-b5c252e50527

--- scripts/items/speed-potion.lua
+++
@@ -1,6 +0,0 @@
-require "scripts/lua/npclib"
-
-function use(user)
-	  mana.being_set_speed(user, 10.0) -- 10 Tiles pro Sekunde (normalerweise 6)
-	  mana.being_apply_status(user, 3, 150) -- Entfernt die Geschwindigkeit später wieder
-end
