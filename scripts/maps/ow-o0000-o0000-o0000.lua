f979ff0660e5325d2bf434e90bedbcfe9ea189e6
seeseekey <seeseekey@784bf6b6-0765-11df-962c-b5c252e50527>
2011-08-19 10:50:26 +0000

    Schild umformuliert.
    git-svn-id: https://invertika.googlecode.com/svn/trunk@5230 784bf6b6-0765-11df-962c-b5c252e50527

--- scripts/maps/ow-o0000-o0000-o0000.lua
+++ scripts/maps/ow-o0000-o0000-o0000.lua
@@ -25,7 +25,7 @@ atinit(function()
  sign.create_sign(113, 84, "Frisörsalon Umet\
 Schneiden, Färben, Waschen, Perücken und mehr.") -- Schild vor dem Friseur
  sign.create_sign(131, 82, "Feinste Waffen zu günstigen Preisen\
-Wir müssen darauf hinweisen, dass bei Diebstahl ein Ladenverbot verhängt wird sowie die Hand als Entschädigung dabehalten wird.") -- Schild vor dem Waffenladen
+Wir müssen darauf hinweisen, dass bei Diebstahl ein Ladenverbot verhängt wird sowie die Hand als Entschädigung einbehalten wird.") -- Schild vor dem Waffenladen
  sign.create_sign(43, 107, "Kein Trinkwasser!") -- Schild vor dem Brunnen
  sign.create_sign(146, 160, "Baden und Angeln verboten!") -- Schild vor dem Wasserloch
 
