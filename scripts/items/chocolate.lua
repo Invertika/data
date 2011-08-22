34566c3e7d5cc22c04d99eaac32edc08feb676da
seeseekey <seeseekey@784bf6b6-0765-11df-962c-b5c252e50527>
2011-08-22 14:28:39 +0000

    Itemskript erweitert.
    git-svn-id: https://invertika.googlecode.com/svn/trunk@5248 784bf6b6-0765-11df-962c-b5c252e50527

--- scripts/items/chocolate.lua
+++ scripts/items/chocolate.lua
@@ -5,12 +5,14 @@ function use(user)
     local count = invertika.get_quest_status(ch, quest_string) + 1
     invertika.set_quest_status(ch, quest_string, count) 
 
-    if count >=20 then
+    if count >=15 then
         mana.being_say(user, invertika.get_random_element(
           "Und wieder tausende Kilokalorien.",
           "Mist, ich wollte doch nicht mehr so viel essen!",
           "Schon wieder Schokolade? Ich will Gurken!",
-          "Schokolade, lecker - *mampf*"))
+          "Schokolade, lecker - *mampf*",
+		  "Schokolade macht glücklich, wie traurig.",
+		  "Noch drei Stück und ich kann meine Diät vergessen..."))
         invertika.set_quest_status(ch, quest_string, 0)
     end
 end
