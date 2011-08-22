4f0b8dbb961c53fa0b9e98586b72ebee4def9e7d
Jurkan@gmx.de <Jurkan@gmx.de@784bf6b6-0765-11df-962c-b5c252e50527>
2011-08-22 08:42:36 +0000

    Schokolade macht jetzt WIRKLICH dick.
    git-svn-id: https://invertika.googlecode.com/svn/trunk@5244 784bf6b6-0765-11df-962c-b5c252e50527

--- scripts/items/chocolate.lua
+++ scripts/items/chocolate.lua
@@ -1,16 +1,16 @@
 function use(user)
-	local quest_string = "character_chocolate_count"
+    local quest_string = "character_chocolate_count"
     invertika.init_quest_status(ch, quest_string)
     
-	local count = invertika.get_quest_status(ch, quest_string)
-    count=count+1;
-	
-	if count >=25 then
-	  mana.being_say(user, invertika.get_random_element(
-      "Und wieder tausende Kilokalorien."
-      "Mist, ich wollte doch nicht mehr so viel essen!",
-	  "Schon wieder Schokolade? Ich will Gurken!",
-	  "Schokolade, lecker - *mampf*"))
-	  invertika.set_quest_status(ch, quest_string, 0)
-	end
+    local count = invertika.get_quest_status(ch, quest_string) + 1
+    invertika.set_quest_status(ch, quest_string, count) 
+
+    if count >=20 then
+        mana.being_say(user, invertika.get_random_element(
+          "Und wieder tausende Kilokalorien."
+          "Mist, ich wollte doch nicht mehr so viel essen!",
+          "Schon wieder Schokolade? Ich will Gurken!",
+          "Schokolade, lecker - *mampf*"))
+        invertika.set_quest_status(ch, quest_string, 0)
+    end
 end
