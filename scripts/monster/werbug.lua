6c07c40175db2576285acae027ef228835bee626
ablu.erikschilling@gmail.com <ablu.erikschilling@gmail.com@784bf6b6-0765-11df-962c-b5c252e50527>
2011-09-01 09:02:51 +0000

    Formatierung angepasst.
    git-svn-id: https://invertika.googlecode.com/svn/trunk@5280 784bf6b6-0765-11df-962c-b5c252e50527

--- scripts/monster/werbug.lua
+++ scripts/monster/werbug.lua
@@ -14,14 +14,14 @@ function update(mob)
   local r = math.random(0, 200);
   if r == 0 then
     mana.being_say(mob, invertika.get_random_element(
-          "Schlangeneier beim Großmarkthändler Fritz, ganz günstig nur jetzt.",
-          "Extrasalat, Extrasalat nur jetzt für 99 Aki.",
-          "Bezahlen sie heute, erhalten sie ihre Ware später, nur jetzt!",
-          "Diese Anzeige wird ihnen präsentiert von Lokomodorol Injektin Tabletten.",
-          "Nach dem nächsten Piepton, kaufen sie Energetika.",
-		  "Hop und Ex und der Tag ist ein Klecks.",
-		  "Nur bei ihrer Zentralbank, Zinsen auf ihr eingezahltes Guthaben. Eröffnet jetzt ein Konto.",
-		  "Gestresst? Dann ab in den Traumurlaub nach Quakari. Nur jetzt für 999 Aki."))
+      "Schlangeneier beim Großmarkthändler Fritz, ganz günstig nur jetzt.",
+      "Extrasalat, Extrasalat nur jetzt für 99 Aki.",
+      "Bezahlen sie heute, erhalten sie ihre Ware später, nur jetzt!",
+      "Diese Anzeige wird ihnen präsentiert von Lokomodorol Injektin Tabletten.",
+      "Nach dem nächsten Piepton, kaufen sie Energetika.",
+      "Hop und Ex und der Tag ist ein Klecks.",
+      "Nur bei ihrer Zentralbank, Zinsen auf ihr eingezahltes Guthaben. Eröffnet jetzt ein Konto.",
+      "Gestresst? Dann ab in den Traumurlaub nach Quakari. Nur jetzt für 999 Aki."))
   end
 end
 
