641d6c645c6d049f99c6831696fb786e4d8d4681
Jurkan@gmx.de <Jurkan@gmx.de@784bf6b6-0765-11df-962c-b5c252e50527>
2011-09-26 09:21:21 +0000

    Geheimshop implementiert
    git-svn-id: https://invertika.googlecode.com/svn/trunk@5392 784bf6b6-0765-11df-962c-b5c252e50527

--- scripts/maps/iw-selphi-timlet-escape-tunnel2-o00-o00-n01.lua
+++ scripts/maps/iw-selphi-timlet-escape-tunnel2-o00-o00-n01.lua
@@ -16,7 +16,33 @@
 
 
 require "scripts/lua/npclib"
-
+require "scripts/libs/sign"
 
 atinit(function()
+    sign.create_sign(24, 7, "Gerhards ganz gewöhnlicher Gebrauchtwarenladen \
+\
+Ganz bestimmt nicht geheim oder schwer zu finden oder so ^^\
+Deshalb wird hier auch nicht mit illegaler Ware gehandelt.");
+    create_npc("Gerhard", 7, 27 * TILESIZE + 16, 5 * TILESIZE + 16, gerhard_talk, nil) --- Gerhard
 end)
+
+function gerhard_talk(npc, ch)
+    -- Dieser Händler könnte später mal Speed verkaufen.
+    do_message(npc, ch, "Bist du ein Mitglied der Stadtwache? Nein? Sehr gut, was kann ich für dich tun?")
+    while true do 
+        local v = do_choice(npc, ch, "Kaufen.",
+                                     "Verkaufen.",
+                                     "Nichts. Danke.")
+        if v == 1 then
+            mana.npc_trade(npc, ch, false, { {30025, 10, 2000}, {30003, 10, 300}, {20023, 10, 12490} })
+            break
+        elseif v == 2 then
+            mana.npc_trade(npc, ch, true)
+            break
+        elseif v == 3 then
+            do_message(npc, ch, "Wenn die Stadtwache wegen dir von mir erfährt, kannst du dir gleich schonmal deinen Grabstein aussuchen!")
+            break
+        end
+    end
+    do_npc_close(npc, ch)
+end
