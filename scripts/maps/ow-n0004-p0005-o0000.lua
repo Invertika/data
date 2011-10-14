02fcd8b28418eba3e63945ae76b0b7e07b603286
panzer1996@gmail.com <panzer1996@gmail.com@784bf6b6-0765-11df-962c-b5c252e50527>
2011-10-14 16:35:56 +0000

    Schild eine Beschriftung gegeben.
    git-svn-id: https://invertika.googlecode.com/svn/trunk@5467 784bf6b6-0765-11df-962c-b5c252e50527

--- scripts/maps/ow-n0004-p0005-o0000.lua
+++ scripts/maps/ow-n0004-p0005-o0000.lua
@@ -19,5 +19,7 @@ require "scripts/lua/npclib"
 dofile("data/scripts/libs/warp.lua")
 
 atinit(function()
- create_inter_map_warp_trigger(nil, 33, 43, 55) --- Intermap warp
+   create_inter_map_warp_trigger(nil, 33, 43, 55) --- Intermap warp
+   sign_entrance = "Burg Cedric"
+   sign.create_sign(106, 182, sign_entrance) --- Schild Burgeingang
 end)
