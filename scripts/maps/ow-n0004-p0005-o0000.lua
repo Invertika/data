42d7a40781bb74effb5b474b86d9373581d48750
ablu.erikschilling@gmail.com <ablu.erikschilling@gmail.com@784bf6b6-0765-11df-962c-b5c252e50527>
2011-10-15 11:56:46 +0000

    Burg Cedric: Marktschreier aufgestellt. TODO: NPC-Sprite austauschen.
    git-svn-id: https://invertika.googlecode.com/svn/trunk@5495 784bf6b6-0765-11df-962c-b5c252e50527

--- scripts/maps/ow-n0004-p0005-o0000.lua
+++ scripts/maps/ow-n0004-p0005-o0000.lua
@@ -15,15 +15,50 @@
 ----------------------------------------------------------------------------------
 
 require "scripts/lua/npclib"
-require "scripts/libs/sign"
+
+require "scripts/libs/invertika"
 require "scripts/libs/nethek"
+require "scripts/libs/sign"
 
 dofile("data/scripts/libs/warp.lua")
 
 atinit(function()
-   create_inter_map_warp_trigger(nil, 33, 43, 55) --- Intermap warp
-   nethek.create_netheksaeule(103 * TILESIZE, 107 * TILESIZE + 16) ---Netheksäule
+    create_inter_map_warp_trigger(nil, 33, 43, 55) --- Intermap warp
+    nethek.create_netheksaeule(103 * TILESIZE, 107 * TILESIZE + 16) ---Netheksäule
 
-   sign_entrance = "Burg Cedric"
-   sign.create_sign(106, 182, sign_entrance) --- Schild Burgeingang
+    sign_entrance = "Burg Cedric"
+    sign.create_sign(106, 182, sign_entrance) --- Schild Burgeingang
+
+    diem = create_npc("Diem", 120, 130 * TILESIZE + 16, 150 * TILESIZE + 16, diem_talk, diem_update)
+    invertika.create_npc_talk_random(diem,
+        {"Kauft Leute! Kauft!",
+        "Beste Waren! Hergestellt in den königlichen Schmieden!",
+        "Sehen Sie! Dieses Schert ist unzerbrechbar! ZACK. Oh...",
+        "Kommen Sie meine Herren. Diese Waffen sind die besten."})
 end)
+
+function diem_talk(npc, ch)
+    npc.trade(npc, ch, false, {
+        {10001, 30, 50},
+        {10013, 30, 140},
+        {10002, 30, 1000},
+        {10009, 30, 1690},
+        {20001, 30, 250},
+        {20002, 30, 250},
+        {20011, 30, 250},
+        {20012, 30, 250},
+        {20013, 30, 250},
+        {20014, 30, 250},
+        {20015, 30, 250},
+        {20016, 30, 250},
+        {20017, 30, 250},
+        {20018, 30, 250},
+        {20009, 30, 500}
+    })
+    do_npc_close(npc, ch)
+end
+
+function diem_update(npc)
+    npclib.walkaround_wide(npc)
+    invertika.npc_talk_random(npc)
+end
