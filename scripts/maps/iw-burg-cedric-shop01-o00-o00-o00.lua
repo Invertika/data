3da79217693b2edf4df85814270aa1c397ec879e
panzer1996@gmail.com <panzer1996@gmail.com@784bf6b6-0765-11df-962c-b5c252e50527>
2011-10-18 07:22:34 +0000

    Worrany, eine Kräuterhexe, hinzugefügt
    git-svn-id: https://invertika.googlecode.com/svn/trunk@5617 784bf6b6-0765-11df-962c-b5c252e50527

--- scripts/maps/iw-burg-cedric-shop01-o00-o00-o00.lua
+++ scripts/maps/iw-burg-cedric-shop01-o00-o00-o00.lua
@@ -18,4 +18,32 @@ require "scripts/lua/npclib"
 require "scripts/libs/invertika"
 
 atinit(function()
+    create_npc("Worrany", 69, 22 * TILSIZE + 16, 27 * TILESIZE + 16, worrany_talk, worrany_update)
 end)
+
+function worrany_talk(npc, ch)
+    do_message(npc, ch, "Tag")
+    do_npc_close(npc, ch)
+end
+
+local timer = {}
+function worrany_update(npc)
+    if not timer[npc] then
+        timer[npc] = 1
+    end
+    
+    timer = timer + 1
+    local x = (mana.posX(chr) - 16) / 32
+    
+    if (timer % 100 == 0 then
+        if x == 22 then
+            x = 18
+        elseif x == 18 then
+            x = 27
+        else
+            x == 22
+        end
+        mana.being_walk(npc, x * 32 + 16, 880, 2)
+        mana.effect_create(5, (x + 1) * TILESIZE, 28 * TILESIZE)
+    end
+end
