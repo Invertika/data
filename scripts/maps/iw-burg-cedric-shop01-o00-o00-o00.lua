844b0f86a7c39b5fbd249b940639b62998954130
panzer1996@gmail.com <panzer1996@gmail.com@784bf6b6-0765-11df-962c-b5c252e50527>
2011-10-18 08:48:49 +0000

    worrany Kauft/verkauft jetzt Tabletten
    git-svn-id: https://invertika.googlecode.com/svn/trunk@5622 784bf6b6-0765-11df-962c-b5c252e50527

--- scripts/maps/iw-burg-cedric-shop01-o00-o00-o00.lua
+++ scripts/maps/iw-burg-cedric-shop01-o00-o00-o00.lua
@@ -22,7 +22,39 @@ atinit(function()
 end)
 
 function worrany_talk(npc, ch)
-    do_message(npc, ch, "Tag")
+    do_message(npc, ch, "Willkommen, in meinem Kräuterladen")
+	do_message(npc, ch, "Was kann ich für sie tun?")
+    
+	while true do
+        local v = do_choice(npc, ch, "Kaufen.",
+					         "Verkaufen.",
+					         "Nichts. Danke.")
+						 
+	    if v == 1 then
+	        mana.npc_trade(npc, ch, false, {
+            {30015, 12, 25}, 
+            {30016, 12, 50}, 
+            {30017, 12, 75}, 
+            {30018, 12, 25}, 
+            {30019, 12, 55}, 
+            {30020, 12, 75},
+            })
+	        break
+        elseif v == 2 then
+	        mana.npc_trade(npc, ch, true, {
+            {30015, 12, 12}, 
+            {30016, 12, 25}, 
+            {30017, 12, 37}, 
+            {30018, 12, 12}, 
+            {30019, 12, 27}, 
+            {30020, 12, 37},
+            })
+		    break
+	    elseif v == 3 then
+	        do_message(npc, ch, "Beehren sie uns bald wieder.")
+	        break
+	    end
+	end
     do_npc_close(npc, ch)
 end
 
@@ -39,6 +71,7 @@ function worrany_update(npc)
             else
                 x = 22
             end
+        end
         mana.being_walk(npc, x * 32 + 16, 880, 2)
         if x ~= 22 then
             mana.effect_create(5, (x + 1) * TILESIZE, 28 * TILESIZE)
