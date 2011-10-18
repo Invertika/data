f536bd2a57d79de40e4f5e48595ead3fb2c6bdd1
panzer1996@gmail.com <panzer1996@gmail.com@784bf6b6-0765-11df-962c-b5c252e50527>
2011-10-18 13:40:32 +0000

    Worrany sollte jetzt herumlaufen (Dank an jurkan)
    git-svn-id: https://invertika.googlecode.com/svn/trunk@5642 784bf6b6-0765-11df-962c-b5c252e50527

--- scripts/maps/iw-burg-cedric-shop01-o00-o00-o00.lua
+++ scripts/maps/iw-burg-cedric-shop01-o00-o00-o00.lua
@@ -56,26 +56,27 @@ function worrany_talk(npc, ch)
     do_npc_close(npc, ch)
 end
 
--- local walk_timer = {}
--- function worrany_update(npc)
-    -- if walk_timer[npc] then
-        -- walk_timer[npc] = walk_timer[npc] + 1
-        -- local x = mana.posX(chr) / TILESIZE - 0.5
+local walk_timer = {}
+function worrany_update(npc)
+    if walk_timer[npc] then
+        walk_timer[npc] = walk_timer[npc] + 1
+        local x = mana.posX(npc) / TILESIZE - 0.5
         
-        -- if walk_timer[npc] % 100 == 0 then
-            -- if x == 22 then
-                -- x = 18
-            -- elseif x == 18 then
-                -- x = 27
-            -- else
-                -- x = 22
-            -- end
-        -- end
-        -- mana.being_walk(npc, x * 32 + 16, 880, 2)
-        -- if x ~= 22 then
-            -- mana.effect_create(5, (x + 1) * TILESIZE, 28 * TILESIZE)
-        -- end
-    -- else
-        -- walk_timer[npc] = 1
-    -- end
--- end
+        if walk_timer[npc] % 100 == 0 then
+            if x == 22 * TILESIZE + 16 then
+                x = 18
+            elseif x == 18 * TILESIZE + 16 then
+                mana.effect_create(5, (x + 1) * TILESIZE, 28 * TILESIZE)
+                x = 27
+            elseif x == 27 * TILESIZE + 16 then
+                mana.effect_create(5, (x + 1) * TILESIZE, 28 * TILESIZE)
+                x = 22
+            else
+                x = nil
+            end
+            if not(x == nil) then mana.being_walk(npc, x * 32 + 16, 880, 2)
+        end
+    else
+        walk_timer[npc] = 1
+    end
+end
