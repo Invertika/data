3cfc876dfacae7004ef2525a8717d2dd617c30a5
panzer1996@gmail.com <panzer1996@gmail.com@784bf6b6-0765-11df-962c-b5c252e50527>
2011-10-26 12:44:50 +0000

    Ticketverkäufer Geban hinzugefügt
    git-svn-id: https://invertika.googlecode.com/svn/trunk@5766 784bf6b6-0765-11df-962c-b5c252e50527

--- scripts/maps/iw-greganyzoo-house01-o00-o00-o00.lua
+++ scripts/maps/iw-greganyzoo-house01-o00-o00-o00.lua
@@ -18,7 +18,27 @@ require "scripts/lua/npclib"
 require "scripts/libs/invertika"
 
 atinit(function()
+    --TODO change Sprite id
+    create_npc("Geban", 120, 23 * TILESIZE + 16, 27  * TILESIZE + 16, geban_talk, nil)
 end)
 
-function isskel_talk(npc, ch)
+function geban_talk(npc, ch)
+    do_message(npc, ch, "Hallo, was kann ich für sie tuen?")
+    
+        while true do
+        local v = do_choice(npc, ch,
+          "Kaufen.",
+          "Nichts. Danke.")
+                         
+        if v == 1 then
+            mana.npc_trade(npc, ch, false,
+              {10001, 10, 1500}
+              })
+            break
+        elseif v == 2 then
+            do_message(npc, ch, "Wiedersehen.")
+            break
+        end
+    end
+    do_npc_close(npc, ch)
 end
