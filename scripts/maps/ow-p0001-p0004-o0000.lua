6b493cb051c7773dd6f1aaf19c797388f19fcc88
panzer1996@gmail.com <panzer1996@gmail.com@784bf6b6-0765-11df-962c-b5c252e50527>
2011-10-26 05:53:35 +0000

    Trigger für die Wachen hinzugefügt
    git-svn-id: https://invertika.googlecode.com/svn/trunk@5758 784bf6b6-0765-11df-962c-b5c252e50527

--- scripts/maps/ow-p0001-p0004-o0000.lua
+++ scripts/maps/ow-p0001-p0004-o0000.lua
@@ -22,9 +22,13 @@ atinit(function()
  create_inter_map_warp_trigger(77, 87, 75, 65) --- Intermap warp
  nethek.create_netheksaeule(187 * TILESIZE, 173 * TILESIZE + 16) ---Netheks‰ule
  
- create_npc("Wache", 26, 140 * TILESIZE + 16, 180 * TILESIZE + 16, wache_talk, nil) -- Wache Innentor
- create_npc("Wache", 26, 139 * TILESIZE + 16, 185 * TILESIZE + 16, wache_talk, nil) -- Wache Innentor
- create_npc("Wache", 26, 162 * TILESIZE + 16, 185 * TILESIZE + 16, wache_talk, nil) -- Wache Innentor
+ mana.trigger_create(138 * TILESIZE, 176 * TILESIZE, 9 * TILESIZE, 3 * TILESIZE, "wache_trigger", 1, true)
+ mana.trigger_create(135 * TILESIZE, 186 * TILESIZE, 3 * TILESIZE, 4 * TILESIZE, "wache_trigger", 2, true)
+ mana.trigger_create(168 * TILESIZE, 179 * TILESIZE, 3 * TILESIZE, 11 * TILESIZE, "wache_trigger", 3, true)
+ 
+ wache_oben = create_npc("Wache", 26, 140 * TILESIZE + 16, 180 * TILESIZE + 16, wache_talk, nil) -- Wache Innentor
+ create_npc("Wache", 26, 138 * TILESIZE + 16, 186 * TILESIZE + 16, wache_talk, nil) -- Wache Innentor
+ create_npc("Wache", 26, 167 * TILESIZE + 16, 183 * TILESIZE + 16, wache_talk, nil) -- Wache Innentor
 end)
 
 function wache_talk(npc, ch)
@@ -34,4 +38,27 @@ function wache_talk(npc, ch)
 	    do_message(npc, ch, "Viel Spaﬂ.")
     end
     do_npc_close(npc, ch)
+end
+
+function wache_trigger(ch, id)
+    if mana.being_type(ch) ~= TYPE_MONSTER then
+        if mana.chr_inv_count(ch, 40049) == 0 then
+            local x = mana.posX(ch)
+            local y = mana.posY(ch)
+            if y < 179 * TILESIZE + 16 then
+                y = 179 * TILESIZE + 16
+            else if x < 138 * TILESIZE + 16 then
+                x = 138 * TILESIZE + 16
+            else
+                
+            if id == 1 then
+                y = 179 * TILESIZE + 16
+            elseif id == 2 then
+                x = 138 * TILESIZE + 16
+            elseif id == 3 then
+                x = 138 * TILESIZE + 16
+            mana.chr_warp(ch, nil, x, y)
+            mana.being_say(wache_oben, "Nur mit Ticket darfst du in den Zoo")
+        end
+    end
 end
