925d7cc7c97ab5747d948a599d7eebf1bff1e5ab
seeseekey <seeseekey@784bf6b6-0765-11df-962c-b5c252e50527>
2011-10-29 14:10:51 +0000

    Zeiteisen um Ansagefunktion erweitert.
    git-svn-id: https://invertika.googlecode.com/svn/trunk@5838 784bf6b6-0765-11df-962c-b5c252e50527

---
+++ scripts/items/timeiron.lua
@@ -0,0 +1,8 @@
+-- Example use script. Makes the player character say "*munch*munch*munch*" when using this item.
+-- The HP regeneration effect is handled separately based on the heal value in items.xml
+
+require "scripts/libs/datetime"
+
+function use(user)
+	mana.being_say(user, string.format("Es es ist %s Uhr %s im übrigen ist das ein %s.", datetime.get_current_time(), datetime.get_current_date(), datetime.get_current_weekday()))
+end
