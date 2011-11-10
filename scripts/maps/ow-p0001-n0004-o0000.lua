a16354b8d5e5c0e255214f352eba776a54c8f0c5
panzer1996@gmail.com <panzer1996@gmail.com@784bf6b6-0765-11df-962c-b5c252e50527>
2011-11-10 14:30:56 +0000

    Netheksäule hinzuegfügt
    git-svn-id: https://invertika.googlecode.com/svn/trunk@5935 784bf6b6-0765-11df-962c-b5c252e50527

--- scripts/maps/ow-p0001-n0004-o0000.lua
+++ scripts/maps/ow-p0001-n0004-o0000.lua
@@ -15,9 +15,11 @@
 ----------------------------------------------------------------------------------
 
 require "scripts/lua/npclib"
+require "scripts/libs/nethek"
 
 dofile("data/scripts/libs/warp.lua")
 
 atinit(function()
  create_inter_map_warp_trigger(69, 81, 71, 59) --- Intermap warp
+     nethek.create_netheksaeule(97 * TILESIZE, 155 * TILESIZE + 16) ---Netheksäule
 end)
