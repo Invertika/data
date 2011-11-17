51710e809d590d61d6c9461966abfea2cabd33b2
panzer1996@gmail.com <panzer1996@gmail.com@784bf6b6-0765-11df-962c-b5c252e50527>
2011-11-17 19:54:47 +0000

    n4-p3: Eiserkäufer hinzugefügt
    git-svn-id: https://invertika.googlecode.com/svn/trunk@6054 784bf6b6-0765-11df-962c-b5c252e50527

--- scripts/maps/ow-n0004-p0003-o0000.lua
+++ scripts/maps/ow-n0004-p0003-o0000.lua
@@ -20,4 +20,35 @@ dofile("data/scripts/libs/warp.lua")
 
 atinit(function()
  create_inter_map_warp_trigger(43, 31, 41, 53) --- Intermap warp
+ 
+ --TODO Change Sprite ID
+ create_npc("Betirambo", 2, 101 * TILESIZE + 16, 145 * TILESIZE + 16, betirambo_talk, betirambo_update)
 end)
+
+function betirambo_talk(npc, ch)
+    do_message(npc, ch, invertika.get_random_element("Willst du ein Eis haben?",
+      "Ein Eis?"))
+    while true do
+        local v = do_choice(npc, ch, 
+          invertika.get_random_element("Jep",
+          "Jap",
+          "Jo",
+          "Ja"),
+          invertika.get_random_element("Nein",
+          "Nö",
+          "Zu kalt für Eis")
+        if v == 1 then
+            mana.npc_trade(npc, ch, false,
+              30029, 1, 675)
+            break
+        elseif v == 2 then
+            break
+        end
+    end
+    do_message(npc, ch, "Bis bald")
+    do_npc_close(npc, ch)
+end
+
+function betirambo_update(npc)
+    //walkaround_map(npc)
+end
