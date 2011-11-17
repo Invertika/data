d75ff3c9f2898de30a6dcfaf527c5cd17ef09396
panzer1996@gmail.com <panzer1996@gmail.com@784bf6b6-0765-11df-962c-b5c252e50527>
2011-11-17 18:49:20 +0000

    Nelaro gambling house: Fehlendes Koma beim Irac ergänzt, Spieltische hinzugefügt
    git-svn-id: https://invertika.googlecode.com/svn/trunk@6050 784bf6b6-0765-11df-962c-b5c252e50527

--- scripts/maps/iw-nelaro-gambling-house-o00-o00-o00.lua
+++ scripts/maps/iw-nelaro-gambling-house-o00-o00-o00.lua
@@ -24,6 +24,19 @@ require "scripts/libs/roulette"
 atinit(function()
     --TODO Change Sprite ID
     create_npc("Irac", 23, 30 * TILESIZE + 16, 37 * TILESIZE + 16, irac_talk, nil)
+    
+    create_npc("Würfeltisch", 1, 13 * TILESIZE + 16, 35 * TILESIZE + 16, dice.dicemanager_talk, nil)
+    create_npc("Würfeltisch", 1, 20 * TILESIZE + 16, 35 * TILESIZE + 16, dice.dicemanager_talk, nil)
+    create_npc("Würfeltisch", 1, 20 * TILESIZE + 16, 39 * TILESIZE + 16, dice.dicemanager_talk, nil)
+    
+    create_npc("Slotmaschine", 1, 3 * TILESIZE + 16, 35 * TILESIZE + 16, slotmachine.slotmachine_talk, nil)
+    create_npc("Slotmaschine", 1, 16 * TILESIZE + 16, 35 * TILESIZE + 16, slotmachine.slotmachine_talk, nil)
+    create_npc("Slotmaschine", 1, 17 * TILESIZE + 16, 35 * TILESIZE + 16, slotmachine.slotmachine_talk, nil)
+    create_npc("Slotmaschine", 1, 3 * TILESIZE + 16, 40 * TILESIZE + 16, slotmachine.slotmachine_talk, nil)
+    
+    create_npc("Roulette", 1, 5 * TILESIZE + 16, 35 * TILESIZE + 16, roulette.roulette_talk, nil)
+    create_npc("Roulette", 1, 5 * TILESIZE + 16, 39 * TILESIZE + 16, roulette.roulette_talk, nil)
+    create_npc("Roulette", 1, 12 * TILESIZE + 16, 39 * TILESIZE + 16, roulette.roulette_talk, nil)
 end)
 
 function irac_talk(npc, ch)
@@ -43,7 +56,7 @@ function irac_talk(npc, ch)
           "Kaufen",
         invertika.get_random_element("Quasseln",
           "Reden",
-          "Einen Plausch führen.")
+          "Einen Plausch führen."),
         invertika.get_random_element("Nichts",
           "Dies war nur eine Hörprobe!",
           "Ich gehe lieber."))
