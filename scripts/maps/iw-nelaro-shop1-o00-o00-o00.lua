294bcf2c47fffecd5acb0cc561bc7313a5f2bd42
panzer1996@gmail.com <panzer1996@gmail.com@784bf6b6-0765-11df-962c-b5c252e50527>
2011-11-23 15:58:13 +0000

    Waffenladen: Rhosor sollte jetzt Waffen verkaufen
    git-svn-id: https://invertika.googlecode.com/svn/trunk@6183 784bf6b6-0765-11df-962c-b5c252e50527

--- scripts/maps/iw-nelaro-shop1-o00-o00-o00.lua
+++ scripts/maps/iw-nelaro-shop1-o00-o00-o00.lua
@@ -20,5 +20,40 @@ require "scripts/lua/npclib"
 
 atinit(function()
     --TODO Change Sprite ID
-    create_npc("Rhosor", 2, 26 * 32 + 16, 38 * TILESIZE + 16, nil, nil)
+    create_npc("Rhosor", 2, 26 * 32 + 16, 38 * TILESIZE + 16, rhosor_talk, nil)
 end)
+
+function rhosor_talk(npc, ch)
+    do_message(npc, ch, "Tag, Was gibt es?")
+    
+    while true do
+        local v = do_choice(npc, ch,
+          "Kaufen.",
+          "Verkaufen.",
+          "Nichts. Danke.")
+                         
+        if v == 1 then
+            mana.npc_trade(npc, ch, false,
+              {{10001, 20, 50},
+              {10002, 60, 1000},
+              {10009, 60, 1000},
+              {10011, 100, 2200},
+              {10013, 20, 140}
+              })
+            break
+        elseif v == 2 then
+            mana.npc_trade(npc, ch, true,
+              {{10001, 20, 5},
+              {10002, 60, 100},
+              {10009, 60, 100},
+              {10011, 100, 220},
+              {10013, 20, 14}
+              })
+            break
+        elseif v == 3 then
+            do_message(npc, ch, "Beehren sie uns bald wieder.")
+            break
+        end
+    end
+    do_npc_close(npc, ch)
+end
