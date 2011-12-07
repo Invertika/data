8e3061cd982ab1542ff2b51a4e09db69cb98eda8
Postremus <panzer1996@googlemail.com>
2011-12-07 19:00:28 +0100

    Nelaro Shop02: Enla sollte jetzt Sachen Verkaufen/Ankaufen. Sie hat genau die gleichen Waren im Angebot, wie Julia in St.

--- scripts/maps/iw-nelaro-shop2-o00-o00-o00.lua
+++ scripts/maps/iw-nelaro-shop2-o00-o00-o00.lua
@@ -19,9 +19,56 @@ require "scripts/lua/npclib"
 require "scripts/libs/invertika"
 
 atinit(function()
-    create_npc("Enla", 4, 33 * TILESIZE + 16, 36 * TILESIZE + 16, nil, nil)
+    create_npc("Enla", 4, 33 * TILESIZE + 16, 36 * TILESIZE + 16, enla_Talk, nil)
 end)
 
-function enla_talk()
-{
-}
+function enla_talk(npc, ch)
+    do_message(npc, ch, invertika.get_random_element("Hi, was kann ich für sie tuen?",
+      "Hi, was gibt es?",
+      "Hi, wie kann ich ihnen helfen?",
+      "Hallo, was kann ich für sie tuen?",
+      "Hallo, was gibt es?",
+      "Hallo, wie kann ich ihnen helfen?",
+      "Guten Tag, was kann ich für sie tuen?",
+      "Guten Tag, was gibt es?",
+      "Guten Tag, wie kann ich ihnen helfen?",
+      "Moin",
+      "Was wollen sie hier?!"))
+      
+    while true do
+        local v = do_choice(npc, ch, "Kaufen.",
+          "Verkaufen.",
+          "Nichts, Danke.")
+        
+        if v == 1 then
+            mana.npc_trade(npc, ch, false, {
+              {10001, 10, 19}, 
+              {10013, 10, 159}, 
+              {10002, 10, 997}, 
+              {20001, 10, 30}, 
+              {20002, 10, 50}, 
+              {20011, 10, 30}, 
+              {20012, 10, 30}, 
+              {20013, 10, 30}, 
+              {20014, 10, 30}, 
+              {20015, 10, 50}, 
+              {20016, 10, 50}, 
+              {20017, 10, 50}, 
+              {20018, 10, 50}
+              })
+            break
+        elseif v == 2 then
+            mana.npc_trade(npc, ch, true)
+            break
+        elseif v == 3 then
+            do_message(npc, ch, invertika.get_random_element("Tschüß",
+              "Tschau",
+              "Bis Bald",
+              "Beehren sie uns bald wieder",
+              "Bis irgendwann.",
+              "Verschwinde!",
+              "Ciao"))
+              break
+        end
+        do_npc_close(npc, ch)
+end
