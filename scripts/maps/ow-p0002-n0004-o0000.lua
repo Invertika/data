b647fc3370315062b587aa57fea5c88f411aeb8c
Postremus <panzer1996@googlemail.com>
2011-12-20 06:51:23 +0100

    Wüstenlager: Kaler, ein Wasserhändler, hinzugefügt. Dieser wird für die Wasserbeschaffungsquest gebaucht. Er verkauft das Wasser für 40 Aki je Flasche.

--- scripts/maps/ow-p0002-n0004-o0000.lua
+++ scripts/maps/ow-p0002-n0004-o0000.lua
@@ -15,9 +15,18 @@
 ----------------------------------------------------------------------------------
 
 require "scripts/lua/npclib"
+require "scripts/libs/invertika"
 
 require "scripts/libs/warp"
 
 atinit(function()
  create_inter_map_warp_trigger(80, 92, 82, 70) --- Intermap warp
+ create_npc("Kaler", 36, 79 * TILESIZE + 16, 136 * TILESIZE + 16, kaler_talk, nil)
 end)
+
+function kaler_talk(npc, ch)
+    mana.npc_trade(npc, ch, false, {
+      {30037, 5, 40}
+    })
+    do_npc_close(npc, ch)
+end
