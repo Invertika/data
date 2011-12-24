193ae5dd93680bd78707314565ba2489e56fdfa4
Postremus <panzer1996@googelmail.com>
2011-12-24 23:22:59 +0100

    Nelaro House04: Den Leuten dort bessere Namen gegeben

--- scripts/maps/iw-nelaro-house04-o00-o00-o00.lua
+++ scripts/maps/iw-nelaro-house04-o00-o00-o00.lua
@@ -21,8 +21,8 @@ require "scripts/libs/invertika"
 atinit(function()
     --TODO: bessere Namen
     create_npc("Koch", 52, 33 * TILESZIE + 16, 38 * TILESIZE + 16, nil, nil);
-    create_npc("Vater", 60, 20 * TILESZIE + 16, 37 * TILESZIE + 16, nil, nil)
-    create_npc("Kind1", 117, 2 * TILESZIE + 16, 31 * TILESZIE + 16, nil, nil)
-    create_npc("Kind2", 17, 19 * TILESZIE + 16, 29 * TILESZIE + 16, nil, nil)
-    cretae_npc("Mutter", 4, 32 * TILESZIE + 16, 30 * TILESZIE + 16, nil, nil)
+    create_npc("Ghang", 60, 20 * TILESZIE + 16, 37 * TILESZIE + 16, nil, nil)
+    create_npc("Estform", 117, 2 * TILESZIE + 16, 31 * TILESZIE + 16, nil, nil)
+    create_npc("Ingawu", 17, 19 * TILESZIE + 16, 29 * TILESZIE + 16, nil, nil)
+    cretae_npc("Ackona", 4, 32 * TILESZIE + 16, 30 * TILESZIE + 16, nil, nil)
 end)
