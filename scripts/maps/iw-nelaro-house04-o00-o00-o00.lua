e6007ace550a9f9a6d3fc29c7e9d8f558441d641
Erik Schilling <ablu.erikschilling@googlemail.com>
2012-01-07 06:58:33 +0800

    Typo gefixt

--- scripts/maps/iw-nelaro-house04-o00-o00-o00.lua
+++ scripts/maps/iw-nelaro-house04-o00-o00-o00.lua
@@ -22,7 +22,7 @@ atinit(function()
     --TODO: bessere Namen
     create_npc("Koch", 52, 33 * TILESZIE + 16, 38 * TILESIZE + 16, nil, nil);
     create_npc("Ghang", 60, 20 * TILESZIE + 16, 37 * TILESZIE + 16, nil, nil)
-    create_npc("Estform", 117, 2 * TILESZIE + 16, 31 * TILESZIE + 16, nil, nil)
+    create_npc("Estform", 117, 2 * TILESZIE + 16, 31 * TILESIZE + 16, nil, nil)
     create_npc("Ingawu", 17, 19 * TILESZIE + 16, 29 * TILESZIE + 16, nil, nil)
     cretae_npc("Ackona", 4, 32 * TILESZIE + 16, 30 * TILESZIE + 16, nil, nil)
 end)
