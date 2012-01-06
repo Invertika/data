6bb79a918f7f2776edf71372b543bf3688014f82
jurkan <Jurkan@gmx.de>
2012-01-06 23:41:48 +0100

    Einrückung korrigiert

--- scripts/items/pangalactic-gargle-blaster.lua
+++ scripts/items/pangalactic-gargle-blaster.lua
@@ -2,20 +2,20 @@ require "scripts/lua/npclib"
 require "scripts/libs/invertika"
 
 function use(user)
-	mana.being_say(user, invertika.get_random_element("Galaktisch...",
-	  "Jaaa die Antwort ist 42...",
-	  "Die Frage ist so simpel...",
-	  "Wuuuuuuuuuuuuuush",
-	  "Wo ist mein Bademantel?",
-	  "Wieviel ist sieben mal sechs?",
+    mana.being_say(user, invertika.get_random_element("Galaktisch...",
+      "Jaaa die Antwort ist 42...",
+      "Die Frage ist so simpel...",
+      "Wuuuuuuuuuuuuuush",
+      "Wo ist mein Bademantel?",
+      "Wieviel ist sieben mal sechs?",
       "Morgen schwebe ich!"))
-	  
-	  local x = mana.posX(user) + 128
-	  local y = mana.posY(user)
-	  
-	  local save = mana.being_get_speed(user)
-	  
-	  mana.being_walk(user, x , y , 400 )
-	  
-	  mana.being_set_speed(user, save)
+
+    local x = mana.posX(user) + 128
+    local y = mana.posY(user)
+
+    local save = mana.being_get_speed(user)
+
+    mana.being_walk(user, x , y , 400 )
+
+    mana.being_set_speed(user, save)
 end
