262a230277fcce58e2d9d30e9a64dca2ae23d89a
Postremus <panzer1996@googlemail.com>
2012-01-28 18:19:49 +0100

    n4-p1: Mikel -> Bolum, dem NPC Txt gegeben

--- scripts/maps/ow-n0004-p0001-o0000.lua
+++ scripts/maps/ow-n0004-p0001-o0000.lua
@@ -21,5 +21,34 @@ require "scripts/libs/warp"
 
 atinit(function()
  create_inter_map_warp_trigger(41, 29, 39, 51) --- Intermap warp
- create_npc("Mikel", 42, 26 * TILESIZE + 16, 59 * TILESIZE + 16, nil, nil)
+ create_npc("Bolum", 42, 26 * TILESIZE + 16, 59 * TILESIZE + 16, nil, nil)
 end)
+
+function mikel_talk(npc, ch)
+    local queststring = "nelaro_vodel_quests"
+    
+    --Init Quest
+    invertika.init_quest_status(ch, queststring)
+    --Get Quest
+    local quest_var = invertika.get_quest_status(ch, queststring)
+    
+    if quest_var == 0 then
+        do_message(npc, ch, "..")
+    end
+    
+    if quest_var == 7 then
+        do_message(npc, ch, "ah, der alte Vodel hat dich geschickt.")
+        do_message(npc, ch, "Du wilst wahrscheinlich die Antwort auf Blubb wissen")
+        do_message(npc, ch, "Sie lautet blubb")
+        do_message(npc, ch, "Richte ihm bitte schöne Grüße aus.")
+        
+        --Set Quest
+        --Set Quest
+        invertika.set_quest_status(ch, queststring_vodel, 7) --Es wurde schon mit dem Vodel geraedet, die Quest angenommen, die zweite Runde ist begonnen und es wurde mit Bolum geredet.
+    end
+    
+    if quest_var > 7
+        do_message(npc, ch, "Hi.")
+    end
+    do_npc_close(npc, ch)
+end
