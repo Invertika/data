5dd004233be6c79dc62d130a63bc76f62ced75fc
Postremus <panzer1996@googlemail.com>
2012-04-02 13:17:06 +0200

    Nelaro house13 o00-p1-o00: lerre Innenwohnung hinzugefügt, iw-big, iw-middle und iw-small: Die obere Wand ist jetzt im 2. Ground. Dadurch sollte sich eine kleinere Unschönheit beseitigen

---
+++ scripts/maps/iw-nelaro-house13-o00-p01-o00.lua
@@ -0,0 +1,15 @@
+-- Externe Map Skripting Datei
+-- In dieser Datei stehen die entsprechenden externen NPCs, Trigger und anderer Dinge.
+--
+-- © 2008-2011 by The Invertika Development Team
+--
+-- This file is part of Invertika. Invertika is free software; you can redistribute 
+-- it and/or modify it under the terms of the GNU General  Public License as published 
+-- by the Free Software Foundation; either version 3 of the License, or any later version.
+
+
+require "scripts/lua/npclib"
+
+
+atinit(function()
+end)
