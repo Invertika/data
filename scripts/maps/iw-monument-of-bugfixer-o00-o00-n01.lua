ee9bd62b409ffb31c6bb945b6fe642672a28451c
Postremus <panzer1996@googlemail.com>
2012-04-05 12:23:27 +0200

    Denkmal des Fehlerbehebers: Eine Sperre vor dem Eingang in das Denkmal eingebaut. 			Da bin ich zurzeit noch am Überlegen, was als Sperre dienen könnte.
    Monument o0 o0 n1: Ein kleines Mädchen. hinzugefügt.
    Sie steht vor der Statue des Fehlerbehebers.
    Da sie die statue schmücken will, bittet sie um eine Blume.
    Die Id der Blume muss noch angepasst werden, da bis jetzt keine als Item vorhanden ist.

--- scripts/maps/iw-monument-of-bugfixer-o00-o00-n01.lua
+++ scripts/maps/iw-monument-of-bugfixer-o00-o00-n01.lua
@@ -14,4 +14,53 @@ require "scripts/libs/nethek"
 
 atinit(function()
     nehtek.create_netheksaeule(47 * TILESIZE, 8 * TILESIZE + 16)
+    
+    --TODO: bessere Namen
+    create_npc("kleines Mädchen", 1, 48 * TILESIZE + 16, 16 * TILEISZE + 16, maedchen_talk, nil)
 end)
+
+function maedchen_talk(npc, ch)
+    local queststring = "monument_of_bugfixer_flower_quest"
+    
+    --Init Quest
+    invertika.init_quest_status(ch, queststring)
+    
+    --Get Quest
+    local quest_var = invertika.get_quest_status(ch, queststring)
+    
+    if quest_var == 0 then
+        do_message(npc, ch, "Das da hinter uns ist der Große Felerbeheber.")
+        do_message(npc, ch, "Wie gerne würde ich seine Statue mit einer Blum dekorieren.")
+        do_message(npc, ch, "Leider wachsen bei uns keine.")
+        do_message(npc, ch, "Und einfach rausgehen ist nicht.")
+        do_message(npc, ch, "Unsere Augen ahben sich zu stark an die hier unten herschende Dunkelheit gewhönt, sagt zumindest jeder..")
+        do_message(npc, ch, "Du siehst aus wie jemand, der von weit herkommt.")
+        do_message(npc, ch, "Würdest du mir bitte eine bringen?")
+    
+        while true do
+            local c = do_choice(npc, ch, "Ja, natürlich.",
+              "Nein, sorry")
+            if c == 1 then
+                do_message(npc, ch, "Ich danke dir.")
+                --Set Quest
+                invertika.set_quest_status(ch, queststring, 1)
+            elseif c == 2 then
+                do_message(npc, ch, "ok")
+            end
+        end
+    
+    elseif quest_var == 1 then
+        --TODO: Auf die richtige Id der BLume ändern, ist jetzt nur testweise so.
+        if mana.chr_inv_count(ch, 20031) > 0 then
+            do_message(npc, ch, "Ich danke dir.")
+            --Set Quest
+            invertika.set_quet_status(ch, queststring, 2)
+            --TODO: Behlonung ??
+        elseif mana.chr_inv_count(ch, 20031) == 0 then
+            do_message(npc, ch, "Bitte, bringe mir die Blume.")
+        end
+    elseif quest_var == 2 then
+        do_message(npc, ch, "Sieht die Statue nicht schön aus mit der Blume?")
+    end
+    do_np_close(npc, ch)
+end
--- scripts/maps/ow-p0002-n0002-o0000.lua
+++ scripts/maps/ow-p0002-n0002-o0000.lua
@@ -16,10 +16,26 @@
 
 require "scripts/lua/npclib"
 require "scripts/libs/sign"
+require "scripts/libs/invertika"
 
 require "scripts/libs/warp"
 
 atinit(function()
   create_inter_map_warp_trigger(78, 90, 80, 68) --- Intermap warp
   sign.create_sign(147, 79, "Denkmal des großen Fehlerbehebers")
+  
+  mana.trigger_create(36 * TILESIZE, 97 * TILESIZE, 2 * TILESIZE, 2 * TILEISZE, "statue_trigger", true);
+  
+  create_npc("Statue", 1, 36 * TILESIZE + 16, 93 * TILESIZE + 16, statue_talk, nil)
 end)
+
+function statue_talk(npc, ch)
+    
+end
+
+function statue_trigger(ch, id)
+    mana.chatmessage(ch, "Ab hier beginnt das Denkmal des großen Fehlerbehebers.")
+    mana.chatmessage(ch, "Du darfst erst passieren, sobald du dich als würdig erwiesen hast!")
+    
+    mana.chr_warp(ch, nil, 33 * TILESIZE + 16, mana.posY(ch))
+end
