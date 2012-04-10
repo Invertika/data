7ae0614c2f737007b8f1443f07430b2ea5bf4906
Postremus <panzer1996@googlemail.com>
2012-04-10 17:25:27 +0200

    Npc auf Monument-of-bugifexer-o0-o0-n1 korrigert Das kleine Mädchen heißt jetzt Priscilla. Das ihr gegebene Sprite ist noch vorläufig, bis etwas besseres gefunden wurde.

--- scripts/maps/iw-monument-of-bugfixer-o00-o00-n01.lua
+++ scripts/maps/iw-monument-of-bugfixer-o00-o00-n01.lua
@@ -16,7 +16,7 @@ atinit(function()
     nethek.create_netheksaeule(47 * TILESIZE, 8 * TILESIZE + 16)
     
     --TODO: bessere Namen
-    create_npc("kleines Mädchen", 1, 48 * TILESIZE + 16, 16 * TILESIZE + 16, maedchen_talk, nil)
+    create_npc("Priscilla", 18, 48 * TILESIZE + 16, 16 * TILESIZE + 16, maedchen_talk, nil)
     
 end)
 
