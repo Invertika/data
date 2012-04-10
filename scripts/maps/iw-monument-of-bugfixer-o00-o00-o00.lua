7372ca7afe08e25930b0c0277670f1d2ac3cc884
Postremus <panzer1996@googlemail.com>
2012-04-10 10:36:42 +0200

    monument-of-bugifxer-o0-o0-o0: Einen Geheimgang gemappt. In diesem gibt es ein Secret. Dieses gibt einem 250 Geld.

--- scripts/maps/iw-monument-of-bugfixer-o00-o00-o00.lua
+++ scripts/maps/iw-monument-of-bugfixer-o00-o00-o00.lua
@@ -7,9 +7,12 @@
 -- it and/or modify it under the terms of the GNU General  Public License as published 
 -- by the Free Software Foundation; either version 3 of the License, or any later version.
 
+require "scripts/libs/invertika"
+require "scripts/libs/secrets"
 
 require "scripts/lua/npclib"
 
 
 atinit(function()
+  secrets.create_secret(16, 62, "MAP20255_1", nil, nil, 250)
 end)
