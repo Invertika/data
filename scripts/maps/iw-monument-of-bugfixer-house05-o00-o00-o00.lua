d290df8637e2b41788d8d9f3e3a7d75dc05899ec
Postremus <panzer1996@googlemail.com>
2012-04-11 09:32:05 +0200

    Mounument-of-bugfixer-house05: Umelt, denVat von Priscilla, hinzugefügt Das Sprite ist auch hier erstmal nur vorläufig, vielliecht findet sich ja noch as besseres. Umelt soll später auch eine Quest haben. Diese kann aber erst gestartet werden, sobald die Blumen-Quest von Priscilla erledigt wurde.

--- scripts/maps/iw-monument-of-bugfixer-house05-o00-o00-o00.lua
+++ scripts/maps/iw-monument-of-bugfixer-house05-o00-o00-o00.lua
@@ -9,7 +9,26 @@
 
 
 require "scripts/lua/npclib"
-
+require "scripts/libs/invertika"
 
 atinit(function()
+  create_npc("Umach", 111, 13 * TILESIZE + 16, 25 * TILESIZE + 16, umach_talk, nil)
 end)
+
+function umach_talk(npc, ch)
+    local queststring = "monument_of_bugfixer_flower_quest"
+    
+    --Init Quest
+    invertika.init_quest_status(ch, queststring)
+    
+    --Get Quest
+    local quest_var_flower = invertika.get_quest_status(ch, queststring)
+    
+    if quest_var_flower == 0 then
+        do_message(npc, ch, "Fremde hatten wir hier lange nicht mehr.")
+        do_message(npc, ch, "Deshalb können wir euch nicht vertrauen.")
+    elseif quest_var_flower == 1 then
+        do_message(npc, ch, "Platzhalter")
+    end
+    do_npc_close(npc, ch)
+end
