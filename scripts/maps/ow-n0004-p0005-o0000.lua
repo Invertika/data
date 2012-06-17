268b884522467ca792fb42add3c02e4fa44cd4e3
Postremus <panzer1996@googlemail.com>
2012-06-17 13:28:28 +0200

    Burg cedric: Chris Eishohl Quest: Es wird nicht mehr versucht, 5 Eise abzuziehen, obwohl nur eines gehohlt werden sollte

--- scripts/maps/ow-n0004-p0005-o0000.lua
+++ scripts/maps/ow-n0004-p0005-o0000.lua
@@ -161,7 +161,7 @@ function chris_talk(npc, ch)
         if mana.chr_inv_count(ch, 30029) then
             do_message(npc, ch, "Ich bin dir sehr dankbar.")
             invertika.add_money(ch, 400)
-            invertika.add_items(ch, 30029, -5, "Eis")
+            invertika.add_items(ch, 30029, -1, "Eis")
             invertika.set_quest_status(ch, queststring_ice, 2)
         else
             do_mesage(npc, ch, "Eis.")
