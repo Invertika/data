df4d2dd4729139f78dafac6ca7051cbb47c85160
Postremus <panzer1996@googlemail.com>
2012-07-20 18:13:04 +0200

    Burg Cedric Kirche: fehlende schließendeKlammer ergänzt

--- scripts/maps/iw-castle-cedric-church-o00-o00-o00.lua
+++ scripts/maps/iw-castle-cedric-church-o00-o00-o00.lua
@@ -19,6 +19,6 @@ function echver_talk(npc, ch)
     do_message(npc, ch, invertika.get_random_element(
       "Ich bin der Priester dieser Stadt.",
       "Geheiligt sei Invertika, Amoneus und die Bewohner dieses Planeten."
-      "Leider besuchen die Jungen Leute die Kirchen des Landes nicht mehr. Nur noch bei wichtigen Festen sind sie anwesend.");
+      "Leider besuchen die Jungen Leute die Kirchen des Landes nicht mehr. Nur noch bei wichtigen Festen sind sie anwesend."));
     do_npc_close(npc, ch)
 end
