6e990cd0f1a9ec99bbde464fa90ee556fdb2f0a4
Postremus <panzer1996@googlemail.com>
2012-08-11 14:13:49 +0200

    monument of bugfixer: Testweise das Secrets object kommentiert

--- scripts/maps/iw-monument-of-bugfixer-o00-o00-o00.lua
+++ scripts/maps/iw-monument-of-bugfixer-o00-o00-o00.lua
@@ -14,5 +14,5 @@ require "scripts/lua/npclib"
 
 
 atinit(function()
-  secrets.create_secret(16, 62, "MAP20255_1", nil, nil, 250)
+  --secrets.create_secret(16, 62, "MAP20255_1", nil, nil, 250)
 end)
